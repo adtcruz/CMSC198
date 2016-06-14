3ebbc58155b26691412c5b8c9cd421e83ebe9c4d
adtcruz <atcruz1@up.edu.ph>
2016-06-14 13:57:45 +0800

    Added client login handling

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -20,7 +20,7 @@ class Login extends CI_Controller {
 	 */
 	public function index()
 	{
-		//to check if is accessed through POST
+		//to check if is accessed through POST, since this API should only be accessed through AJAX
 		if(!array_key_exists("username",$_POST)){
 			exit('Can not be accessed by any other method');
 		}
@@ -28,7 +28,7 @@ class Login extends CI_Controller {
 		//loads CodeIgniter database module
 		$this->load->database();
 		
-		//queries the database if such a username entered exists
+		//queries the database if such a username entered exists in the technician table
 		$query = $this->db->query("SELECT username FROM technician WHERE username='".$_POST["username"]."'");
 		//sets rows from the db query result array 
 		$rows = $query->result_array();
@@ -63,8 +63,47 @@ class Login extends CI_Controller {
 			}
 		}
 		else {
+			//queries the database if such a username entered exists in the client tables
+			$query = $this->db->query("SELECT username FROM client WHERE username='".$_POST["username"]."'");
+			//sets rows from the db query result array 
+			$rows = $query->result_array();
 			
+			//if there's a single row in the results array, it means the username was found in the client table
+			if(count($rows) == 1){
+				//queries the database if the username and password entered is correct
+				$query = $this->db->query(
+					"SELECT username, givenName, lastName, designation, officeId FROM client WHERE username='".
+					$_POST["username"]."' AND password=SHA1('".$_POST["password"]."')";
+				);
+				//sets rows from the db query result array
+				$rows = $query->result_array();
+				
+				//if there's a single row in the results array, it means the user entered the correct password
+				if(count($rows) == 1){
+					session_start();
+					$_SESSION["type"] = "client";
+					$_SESSION["username"] = $_POST["username"];
+					$_SESSION["givenName"] = $rows[0]['givenName'];
+					$_SESSION["lastName"] = $rows[0]['lastName'];
+					$_SESSION["designation"] = $rows[0]['designation'];
+					//insert place office name here later
+					echo "Logged-on";
+					return;					
+				}
+				
+				//otherwise, the user did not enter the right password
+				else {
+					//send a message that the user did not enter the right password
+					echo "Invalid password";
+					return;
+				}
+			}
+			
+			//else, it's an invalid credentials
+			else {
+				echo "Invalid credentials";
+				return;
+			}
 		}
 	}
-	
 }
