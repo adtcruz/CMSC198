1f59ac1f8e795c4918e511cc7ac77298a42b3fd0
clstia <janelliseurolfan@gmail.com>
2016-06-16 10:29:25 +0800

    MPDF fully okay

--- application/controllers/To_pdf_controller.php
+++ application/controllers/To_pdf_controller.php
@@ -1,23 +1,26 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
  
 class To_pdf_controller extends CI_Controller {
- 
-    public function index()
-    {        
-        $data = [];
 
-        $html = $this->load->view ("To_pdf_view", $data, true);
+	public function __construct ()
+	{
+		parent::__construct ();
+		include APPPATH . 'third_party/MPDF/mpdf.php';
+	}
 
-        //this the the PDF filename that user will get to download
-        $pdfFilePath = "JobRequestForm.pdf";
- 
-        //load mPDF library
-        $this->load->library('m_pdf');
- 
-       //generate the PDF from the given html
-        $this->m_pdf->pdf->WriteHTML($html);
- 
-        //download it.
-        $this->m_pdf->pdf->Output($pdfFilePath, "D");
-    }
+	public function index()
+	{
+		// initialize data here
+		$data = [];
+		// initialize the page to be converted to pdf
+		$html = $this->load->view ("To_pdf_view", $data, true);
+		// set pdf file name
+		$pdfFilePath = "JobRequestForm.pdf";
+		// load mpdf library
+		$this->load->library('m_pdf');
+		// use mpdf function to write pdf
+		$this->m_pdf->pdf->WriteHTML($html);
+		// output the pdf then download
+		$this->m_pdf->pdf->Output($pdfFilePath, "D");
+	}
 }
