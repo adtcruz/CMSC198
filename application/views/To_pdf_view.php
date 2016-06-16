0a38cbe6c83eaea2c40356ea00ca3ad68a5a2936
clstia <janelliseurolfan@gmail.com>
2016-06-16 09:42:58 +0800

    MPDF succcess

--- application/views/To_pdf_view.php
+++ application/views/To_pdf_view.php
@@ -4,9 +4,183 @@
 	<title>sample</title>
 </head>
 <body>
-	<?php 
-	session_start();
-	echo $_SESSION['username']; 
-	?>
+	<table width = "700" border = "2">
+		<tr>
+			<td>
+				<table border = "1" width = "100%">
+						<tr>
+							<td rowspan = "5">
+							</td>
+							<td>
+								UPLB ITC
+							</td>
+							<td>
+								(Accomplish in Duplicate)
+							</td>
+						</tr>
+						<tr>
+							<td>
+								UPLB
+							</td>
+							<td>
+								Job Request No.
+							</td>
+						</tr>
+						<tr>
+							<td>
+								Job Request Form
+							</td>
+							<td>
+								Date
+							</td>
+						</tr>
+						<tr>
+							<td>
+								Tel
+							</td>
+							<td>
+								Time Finished
+							</td>
+						</tr>
+						<tr>
+							<td>
+								Email
+							</td>
+							<td>
+								Time Started
+							</td>
+						</tr>
+				</table>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<table border = "1" width = "100%">
+						<tr>
+							<th colspan = "2">
+								Client Info
+							</th>
+						</tr>
+						<tr>
+							<td>
+								Printed Name:
+							</td>
+							<td>
+								Designation:
+							</td>
+						</tr>
+						<tr>
+							<td>
+								Office / Unit:
+							</td>
+							<td>
+								Tel. No.: 
+							</td>
+						</tr>
+						<tr>
+							<td colspan = "2">
+								Location of Work:
+							</td>
+						</tr>
+						<tr>
+							<td colspan = "2">
+								Work to be done / Problem(s) Encountered:
+							</td>
+						</tr>
+				</table>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<table border = "1" width = "100%">
+						<tr>
+							<th colspan = "4">
+								Services Info
+							</th>
+						</tr>
+						<tr>
+							<td>
+								Description
+							</td>
+							<td>
+								Quantity
+							</td>
+							<td>
+								Unit Cost
+							</td>
+							<td>
+								Total Cost
+							</td>
+						</tr>
+				</table>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<table border = "1" width = "100%">
+					<tr>
+						<th colspan = "4">
+							Bill of Materials
+						</th>
+					</tr>
+					<tr>
+						<td>
+							Description
+						</td>
+						<td>
+							Remarks
+						</td>
+						<td>
+							Quantity
+						</td>
+						<td>
+							Unit
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Materials Used
+						</td>
+						<td> &nbsp; </td>
+						<td> &nbsp; </td>
+						<td> &nbsp; </td>
+					</tr>
+					<tr>
+						<td>
+							Materials Harvested
+						</td>
+						<td> &nbsp; </td>
+						<td> &nbsp; </td>
+						<td> &nbsp; </td>
+						</tr>
+				</table>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<table border = "1" width = "100%">
+					<tr>
+						<th colspan = "4">
+							Recommended Materials / Equipment for Purchase
+						</th>
+					</tr>
+					<tr>
+						<td>
+							Item Specification
+						</td>
+						<td>
+							Quantity
+						</td>
+						<td>
+							Unit
+						</td>
+						<td>
+							Unit Cost
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+	</table>
 </body>
 </html>
