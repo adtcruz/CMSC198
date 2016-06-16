a46c2c13061c4d5fbbaa7cdd6630cd7bf1479cca
clstia <janelliseurolfan@gmail.com>
2016-06-16 09:40:15 +0800

    MPDF succcess

---
+++ application/controllers/To_pdf_controller.php
@@ -0,0 +1,23 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+ 
+class To_pdf_controller extends CI_Controller {
+ 
+    public function index()
+    {        
+        $data = [];
+
+        $html = $this->load->view ("To_pdf_view", $data, true);
+
+        //this the the PDF filename that user will get to download
+        $pdfFilePath = "output_pdf_name.pdf";
+ 
+        //load mPDF library
+        $this->load->library('m_pdf');
+ 
+       //generate the PDF from the given html
+        $this->m_pdf->pdf->WriteHTML($html);
+ 
+        //download it.
+        $this->m_pdf->pdf->Output($pdfFilePath, "D");
+    }
+}
---
+++ application/views/To_pdf_view.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>sample</title>
+</head>
+<body>
+	<?php 
+	session_start();
+	echo $_SESSION['username']; 
+	?>
+</body>
+</html>
