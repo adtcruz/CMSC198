e4a2240481d9ce3cd36877ed9ea92ed640f9f039
adtcruz <atcruz1@up.edu.ph>
2016-06-20 19:56:55 +0800

    Added submission of job requests to DB

--- assets/js/fileJobRequest.controller.js
+++ assets/js/fileJobRequest.controller.js
@@ -2,7 +2,26 @@ $('document').ready(
 	function(){
 		$("#fileJRButton").addClass("light-blue");
 		$("#fileJRButton").addClass("darken-4");
-		$("#fileJRItem").addClass("white-text");
+		$("#fileJRItem").removeClass("black-text");
 		$("#fileJRItem").addClass("white-text");
 	}
 );
+
+function fileJobRequest(url,username,type){
+	jobDesc = problemsEncountered.value;
+	if (jobDesc==="") return;
+	if(type==="client"){
+		$.post(
+			url+'FileJobRequest',
+			{username:username,clientUsername:username,jobDescription:jobDesc},
+			function(data){
+				if (data==="Submitted"){
+					problemsEncountered.value = "";
+					Materialize.toast("Job Request posted.", 3000);
+				}
+			}
+		);
+	}
+	if(type==="technician"){
+	}
+}
