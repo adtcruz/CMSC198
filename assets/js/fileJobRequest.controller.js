75b9bee3fab90bd0fffa38d1146fbc8632d3b563
adtcruz <atcruz1@up.edu.ph>
2016-06-20 16:48:24 +0800

    JS for UI adjustment

---
+++ assets/js/fileJobRequest.controller.js
@@ -0,0 +1,8 @@
+$('document').ready(
+	function(){
+		$("#fileJRButton").addClass("light-blue");
+		$("#fileJRButton").addClass("darken-4");
+		$("#fileJRItem").addClass("white-text");
+		$("#fileJRItem").addClass("white-text");
+	}
+);
