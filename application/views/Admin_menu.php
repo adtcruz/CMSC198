11e8bbd96147cefc64d78d44ce5ba623cb7c57ca
adtcruz <atcruz1@up.edu.ph>
2016-06-20 19:52:04 +0800

    Changed menu item font colour for legibility

--- application/views/Admin_menu.php
+++ application/views/Admin_menu.php
@@ -1,4 +1,4 @@
-<li id="fileJRButton"><a id="fileJRItem" href="#">File Job Request</a></li>
-<li id="viewJRButton"><a id="viewJRItem" href="#">View Job Requests</a></li>
-<li><a id="asgnJRItem" href="#">Assign Job Request</a></li>
-<li><a id="gnrtBlItem" href="#">Generate Bill</a></li>
+<li id="fileJRButton"><a id="fileJRItem" class="waves-effect waves-light black-text" href="#">File Job Request</a></li>
+<li id="viewJRButton"><a id="viewJRItem" class="waves-effect waves-light black-text" href="#">View Job Requests</a></li>
+<li><a id="asgnJRItem" class="waves-effect waves-light black-text" href="#">Assign Job Request</a></li>
+<li><a id="gnrtBlItem" class="waves-effect waves-light black-text" href="#">Generate Bill</a></li>
--- application/views/Client_menu.php
+++ application/views/Client_menu.php
@@ -1,2 +1,2 @@
-<li id="fileJRButton"><a id="fileJRItem" href="<?php base_url();?>file_job">New Job Request</a></li>
-<li id="viewJRButton"><a id="viewJRItem" href="#">View my Job Requests</a></li>
+<li id="fileJRButton"><a id="fileJRItem" class="waves-effect waves-light black-text" href="<?php base_url();?>file_job">New Job Request</a></li>
+<li id="viewJRButton"><a id="viewJRItem" class="waves-effect waves-light black-text" href="#">View my Job Requests</a></li>
