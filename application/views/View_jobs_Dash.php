40decdf55fc27498f87689a2ddf4670b7804caf3
adtcruz <atcruz1@up.edu.ph>
2016-06-21 14:31:11 +0800

    Added view job request page files

---
+++ application/views/View_jobs_Dash.php
@@ -0,0 +1,9 @@
+<div class="row">
+	<?php $this->load->view('Sidenav');?>
+	<!-- FILLER TO PUSH MAIN CONTENT TO THE RIGHT -->
+	<div class="col s3 m3 l3"><br/><br/></div>
+	<!-- MAIN CONTENT CONTAINER -->
+	<div class="col s9 m9 l9">
+		<h5>View Jobs here</h5>
+	</div>
+</div>
---
+++ application/views/View_jobs_script.php
@@ -0,0 +1,1 @@
+<script type="text/javascript" src="<?php base_url();?>assets/js/viewJobRequests.controller.js"></script>
