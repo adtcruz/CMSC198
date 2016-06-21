ff1f0a9499677ae9f5ecbc74c9e8e50e05788ac5
adtcruz <atcruz1@up.edu.ph>
2016-06-21 09:43:38 +0800

    Added safety nets in get_office_list

--- application/controllers/Get_office_list.php
+++ application/controllers/Get_office_list.php
@@ -5,23 +5,34 @@ class Get_office_list extends CI_Controller
 {
 	public function index ()
 	{
-		$this->load->database();
 		session_start();
 		if(array_key_exists("type",$_SESSION)){
 			if(($_SESSION["type"]==="technician")||($_SESSION["type"]==="admin")||($_SESSION["type"]==="superadmin")){
-				$query = $this->db->query("SELECT officeAbbr, officeName FROM office");
+				//loads Code Igniter database module
+				$this->load->database();
+				
+				//queries the database for officeIDs and officeNames
+				$query = $this->db->query("SELECT officeID, officeName FROM office");
+				
+				//gets the results in easy-to-use array form
 				$rows = $query->result_array();
+				//gets the number of rows
 				$rowsN = count($rows);
 				
+				$options = "<option value=\"\" selected=\"selected\">Select office</option>";
+				
 				for($i = 0; $i < $rowsN; $i++){
-					echo "<option value=\"".$rows[$i]["officeAbbr"]."\">".$rows[$i]["officeName"]."</option>";
+					$options = $options."<option value=\"".$rows[$i]["officeID"]."\">".$rows[$i]["officeName"]."</option>";
 				}
+				echo $options;
 			}
 			else {
 				echo "Not allowed for this account";
 			}
 		}
-		
+		else {
+			echo "Not allowed";
+		}
 	}
 }
 ?>
