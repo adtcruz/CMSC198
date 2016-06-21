0eac03d4d472fff4104f05435f1e830fce966102
adtcruz <atcruz1@up.edu.ph>
2016-06-21 19:03:34 +0800

    Added superadmin login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -20,68 +20,68 @@ class Login extends CI_Controller {
 		$this->load->database();
 		
 		//queries the database if such a username entered exists in the technician table
-		$query = $this->db->query("SELECT username FROM adminAcc WHERE username='".$_POST["username"]."'");
+		$query = $this->db->query("SELECT username FROM superAdmin WHERE username='".$_POST["username"]."'");
 		//sets rows from the db query result array
 		$rows = $query->result_array();
-		
-		//if there's a single row in the results array, it means username was found in technician table
 		if(count($rows) == 1){
 		
 			//queries the database to check if the username and password entered is correct
 			$query = $this->db->query(
-				"SELECT username, givenName, lastName FROM adminAcc WHERE username='".
+				"SELECT username, givenName, lastName FROM superAdmin WHERE username='".
 				$_POST["username"]."' AND password=SHA1('".$_POST["password"]."')"
 			);
 			//sets rows from the db query result array
 			$rows = $query->result_array();
-			
+		
 			//if there's a single row in the results array, it means the user entered the correct password
 			if(count($rows) == 1){
 				session_start();
-				$_SESSION["type"] = "technician";
+				$_SESSION["type"] = "superadmin";
 				$_SESSION["username"] = $_POST["username"];
 				$_SESSION["givenName"] = $rows[0]['givenName'];
 				$_SESSION["lastName"] = $rows[0]['lastName'];
 				echo "Logged-on";
 				return;
 			}
-			
+		
 			//otherwise, the user did not enter the right password
 			else {
 				//send a message that the user did not enter the right password
 				echo "Invalid";
 				return;
 			}
+		
 		}
-		else {
-			//queries the database if such a username entered exists in the client tables
-			$query = $this->db->query("SELECT username FROM client WHERE username='".$_POST["username"]."'");
+		
+		else{
+			//queries the database if such a username entered exists in the technician table
+			$query = $this->db->query("SELECT username FROM adminAcc WHERE username='".$_POST["username"]."'");
 			//sets rows from the db query result array
 			$rows = $query->result_array();
-			
-			//if there's a single row in the results array, it means the username was found in the client table
+		
+			//if there's a single row in the results array, it means username was found in technician table
 			if(count($rows) == 1){
-				//queries the database if the username and password entered is correct
+		
+				//queries the database to check if the username and password entered is correct
 				$query = $this->db->query(
-					"SELECT username, givenName, lastName, designation, officeId FROM client WHERE username='".
+					"SELECT username, givenName, lastName, isTechnician FROM adminAcc WHERE username='".
 					$_POST["username"]."' AND password=SHA1('".$_POST["password"]."')"
 				);
 				//sets rows from the db query result array
 				$rows = $query->result_array();
-				
+			
 				//if there's a single row in the results array, it means the user entered the correct password
 				if(count($rows) == 1){
 					session_start();
-					$_SESSION["type"] = "client";
+					if($rows[0]['isTechnician']===1) $_SESSION["type"] = "technician";
+					else $_SESSION["type"] = "admin";
 					$_SESSION["username"] = $_POST["username"];
 					$_SESSION["givenName"] = $rows[0]['givenName'];
 					$_SESSION["lastName"] = $rows[0]['lastName'];
-					$_SESSION["designation"] = $rows[0]['designation'];
-					//insert place office name here later
 					echo "Logged-on";
-					return;					
+					return;
 				}
-				
+			
 				//otherwise, the user did not enter the right password
 				else {
 					//send a message that the user did not enter the right password
@@ -89,11 +89,48 @@ class Login extends CI_Controller {
 					return;
 				}
 			}
-			
-			//else, it's an invalid credential
 			else {
-				echo "Invalid";
-				return;
+				//queries the database if such a username entered exists in the client tables
+				$query = $this->db->query("SELECT username FROM client WHERE username='".$_POST["username"]."'");
+				//sets rows from the db query result array
+				$rows = $query->result_array();
+			
+				//if there's a single row in the results array, it means the username was found in the client table
+				if(count($rows) == 1){
+					//queries the database if the username and password entered is correct
+					$query = $this->db->query(
+						"SELECT username, givenName, lastName, designation, officeId FROM client WHERE username='".
+						$_POST["username"]."' AND password=SHA1('".$_POST["password"]."')"
+					);
+					//sets rows from the db query result array
+					$rows = $query->result_array();
+				
+					//if there's a single row in the results array, it means the user entered the correct password
+					if(count($rows) == 1){
+						session_start();
+						$_SESSION["type"] = "client";
+						$_SESSION["username"] = $_POST["username"];
+						$_SESSION["givenName"] = $rows[0]['givenName'];
+						$_SESSION["lastName"] = $rows[0]['lastName'];
+						$_SESSION["designation"] = $rows[0]['designation'];
+						//insert place office name here later
+						echo "Logged-on";
+						return;					
+					}
+				
+					//otherwise, the user did not enter the right password
+					else {
+						//send a message that the user did not enter the right password
+						echo "Invalid";
+						return;
+					}
+				}
+			
+				//else, it's an invalid credential
+				else {
+					echo "Invalid";
+					return;
+				}
 			}
 		}
 	}
