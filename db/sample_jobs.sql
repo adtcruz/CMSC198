2b31fcaf660c89c56c3925cd3410d16d1066b423
adtcruz <atcruz1@up.edu.ph>
2016-06-21 13:15:47 +0800

    Added job descriptions in sample jobs

--- db/sample_jobs.sql
+++ db/sample_jobs.sql
@@ -2,11 +2,11 @@
 USE cmsc198db;
 
 -- insert pending jobs that are not yet accepted
-INSERT INTO job (startDate, clientID, dateCreated, createdBy, createdByType) values (NULL,1, CURDATE(), 1, 'client');
-INSERT INTO job (startDate, clientID, dateCreated, createdBy, createdByType) values (NULL,1, CURDATE(), 1, 'client');
-INSERT INTO job (startDate, clientID, dateCreated, createdBy, createdByType) values (NULL,1, CURDATE(), 1, 'client');
-INSERT INTO job (startDate, clientID, dateCreated, createdBy, createdByType) values (NULL,1, CURDATE(), 1, 'client');
-INSERT INTO job (startDate, clientID, dateCreated, createdBy, createdByType) values (NULL,2, '20160619', 2, 'client');
-INSERT INTO job (startDate, clientID, dateCreated, createdBy, createdByType) values (NULL,2, '20160619', 2, 'client');
-INSERT INTO job (startDate, clientID, dateCreated, createdBy, createdByType) values (NULL,2, '20160619', 2, 'client');
-INSERT INTO job (startDate, clientID, dateCreated, createdBy, createdByType) values (NULL,2, '20160619', 2, 'client');
+INSERT INTO job (jobDescription, startDate, clientID, dateCreated, createdBy, createdByType) values ("Broken switch.",NULL,1, CURDATE(), 1, 'client');
+INSERT INTO job (jobDescription, startDate, clientID, dateCreated, createdBy, createdByType) values ("No internet.",NULL,1, CURDATE(), 1, 'client');
+INSERT INTO job (jobDescription, startDate, clientID, dateCreated, createdBy, createdByType) values ("Internal Server Errors in uplb.edu.ph",NULL,1, CURDATE(), 1, 'client');
+INSERT INTO job (jobDescription, startDate, clientID, dateCreated, createdBy, createdByType) values ("Liham does not work",NULL,1, CURDATE(), 1, 'client');
+INSERT INTO job (jobDescription, startDate, clientID, dateCreated, createdBy, createdByType) values ("VoIP does not work",NULL,2, '20160619', 2, 'client');
+INSERT INTO job (jobDescription, startDate, clientID, dateCreated, createdBy, createdByType) values ("Windows can not connect",NULL,2, '20160619', 2, 'client');
+INSERT INTO job (jobDescription, startDate, clientID, dateCreated, createdBy, createdByType) values ("High pings on Overwatch",NULL,2, '20160619', 2, 'client');
+INSERT INTO job (jobDescription, startDate, clientID, dateCreated, createdBy, createdByType) values ("Can not access YouTube",NULL,2, '20160619', 2, 'client');
