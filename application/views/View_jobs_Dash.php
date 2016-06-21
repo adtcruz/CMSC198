cd4033d0298d524a4ce562c17bcce56c48346ed1
adtcruz <atcruz1@up.edu.ph>
2016-06-21 16:10:47 +0800

    Added table

--- application/views/View_jobs_Dash.php
+++ application/views/View_jobs_Dash.php
@@ -4,6 +4,16 @@
 	<div class="col s3 m3 l3"><br/><br/></div>
 	<!-- MAIN CONTENT CONTAINER -->
 	<div class="col s9 m9 l9">
-		<h5>View Jobs here</h5>
+		<div class="row">
+			<br/>
+			<h3 class="center-align"><?php if($_SESSION["type"]==="client") echo "My ";?>Job Requests</h3>
+			<br/>
+			<br/>
+			<div class="col s1 m1 l1">&nbsp;</div>
+			<table class="bordered highlight responsive-table col s10 m10 l10">
+				<?php echo $tableC;?>
+			</table>
+			<div class="col s1 m1 l1">&nbsp;</div>
+		</div>
 	</div>
 </div>
