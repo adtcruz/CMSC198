9ef2007c45502cb533f29ffea1d2e894b30c7130
adtcruz <atcruz1@up.edu.ph>
2016-06-21 20:21:35 +0800

    Removed Assign Job in admin menu

--- application/views/Admin_menu.php
+++ application/views/Admin_menu.php
@@ -1,4 +1,3 @@
 <li id="fileJRButton"><a id="fileJRItem" class="waves-effect waves-light black-text" href="<?php base_url();?>new_request">File Job Request</a></li>
 <li id="viewJRButton"><a id="viewJRItem" class="waves-effect waves-light black-text" href="<?php base_url();?>view_jobs">View Job Requests</a></li>
-<li><a id="asgnJRItem" class="waves-effect waves-light black-text" href="#">Assign Job Request</a></li>
 <li><a id="gnrtBlItem" class="waves-effect waves-light black-text" href="#">Generate Bill</a></li>
