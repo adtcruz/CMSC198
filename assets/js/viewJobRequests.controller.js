738232041aaff70f2755996e1be1ca2118e66d11
adtcruz <atcruz1@up.edu.ph>
2016-06-21 14:36:25 +0800

    Added UI adjustment script

---
+++ assets/js/viewJobRequests.controller.js
@@ -0,0 +1,8 @@
+$('document').ready(
+	function(){
+		$("#viewJRButton").addClass("light-blue");
+		$("#viewJRButton").addClass("darken-4");
+		$("#viewJRItem").removeClass("black-text");
+		$("#viewJRItem").addClass("white-text");
+	}
+);
