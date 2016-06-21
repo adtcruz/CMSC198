4e8dd3061f4075671ae6960104197e32d9a020b1
adtcruz <atcruz1@up.edu.ph>
2016-06-21 14:26:49 +0800

    Added route for viewing job requests

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['new_request'] = 'File_JR_controller';
+$route['view_jobs'] = 'View_jobs_controller';
 $route['topdf_bfp'] = 'BillForPayment_controller';
 $route['submit_request'] = 'FileJobRequest';
 $route['topdf_jrf'] = 'JobRequestForm_controller';
