51a4cd2231f0010ad3af3ba0435bf2891666903e
adtcruz <atcruz1@up.edu.ph>
2016-06-21 09:36:09 +0800

    Updated links to new File Job Request page route

--- application/views/Admin_menu.php
+++ application/views/Admin_menu.php
@@ -1,4 +1,4 @@
-<li id="fileJRButton"><a id="fileJRItem" class="waves-effect waves-light black-text" href="<?php base_url();?>file_job">File Job Request</a></li>
+<li id="fileJRButton"><a id="fileJRItem" class="waves-effect waves-light black-text" href="<?php base_url();?>new_request">File Job Request</a></li>
 <li id="viewJRButton"><a id="viewJRItem" class="waves-effect waves-light black-text" href="#">View Job Requests</a></li>
 <li><a id="asgnJRItem" class="waves-effect waves-light black-text" href="#">Assign Job Request</a></li>
 <li><a id="gnrtBlItem" class="waves-effect waves-light black-text" href="#">Generate Bill</a></li>
