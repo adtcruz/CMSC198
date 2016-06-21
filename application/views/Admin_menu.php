7ec4e9b08c73fd3e876b0b3c29ed7741d9af20ef
Mysii <azhaviancadebelen@gmail.com>
2016-06-21 09:47:12 +0800

    tables

--- application/views/Admin_menu.php
+++ application/views/Admin_menu.php
@@ -1,4 +1,4 @@
-<li id="fileJRButton"><a id="fileJRItem" class="waves-effect waves-light black-text" href="<?php base_url();?>file_job">File Job Request</a></li>
-<li id="viewJRButton"><a id="viewJRItem" class="waves-effect waves-light black-text" href="#">View Job Requests</a></li>
-<li><a id="asgnJRItem" class="waves-effect waves-light black-text" href="#">Assign Job Request</a></li>
-<li><a id="gnrtBlItem" class="waves-effect waves-light black-text" href="#">Generate Bill</a></li>
+<li id="fileJRButton"><a id="fileJRItem" class="waves-effect waves-light black-text" href="<?php base_url();?>file_job"><i class="material-icons">note_add</i> File Job Request</a></li>
+<li id="viewJRButton"><a id="viewJRItem" class="waves-effect waves-light black-text" href="#"><i class="material-icons">view_list</i> View Job Requests</a></li>
+<li><a id="asgnJRItem" class="waves-effect waves-light black-text" href="#"><i class="material-icons">assignment</i> Assign Job Request</a></li>
+<li><a id="gnrtBlItem" class="waves-effect waves-light black-text" href="#"><i class="material-icons">payment</i> Generate Bill</a></li>
