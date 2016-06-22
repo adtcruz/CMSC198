5678ba804881438f73419ed21e2468fa1698f3b8
adtcruz <atcruz1@up.edu.ph>
2016-06-22 13:18:41 +0800

    Fixed menu

--- application/views/Admin_menu.php
+++ application/views/Admin_menu.php
@@ -1,5 +1,3 @@
-
-
 <li id="fileJRButton"><a id="fileJRItem" class="waves-effect waves-light black-text" href="<?php base_url();?>new_request">File Job Request</a></li>
 <li id="viewJRButton"><a id="viewJRItem" class="waves-effect waves-light black-text" href="<?php base_url();?>view_jobs">View Job Requests</a></li>
 <li id="viewSchedButton"><a id="viewSchedItem" class="waves-effect waves-light black-text" href="<?php base_url();?>schedule">View Schedule</a></li>
