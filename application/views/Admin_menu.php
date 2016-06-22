761707117ea5ef476f2d488d8ab94835e599a5f0
adtcruz <atcruz1@up.edu.ph>
2016-06-22 09:54:48 +0800

    Fixed merge issue

--- application/views/Admin_menu.php
+++ application/views/Admin_menu.php
@@ -1,6 +1,6 @@
 
 
 <li id="fileJRButton"><a id="fileJRItem" class="waves-effect waves-light black-text" href="<?php base_url();?>new_request">File Job Request</a></li>
-<li id="viewJRButton"><a id="viewJRItem" class="waves-effect waves-light black-text" href="#">View Job Requests</a></li>
+<li id="viewJRButton"><a id="viewJRItem" class="waves-effect waves-light black-text" href="<?php base_url();?>view_jobs">View Job Requests</a></li>
 <li id="viewSchedButton"><a id="viewSchedItem" class="waves-effect waves-light black-text" href="<?php base_url();?>schedule">View Schedule</a></li>
 <li id="genBillButton"><a id="gnrtBlItem" class="waves-effect waves-light black-text" href="<?php base_url();?>generate_bill">Generate Bill</a></li>
