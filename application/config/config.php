578dc4f298406efe3e5f81a1a6cacfbcf5ae93a7
adtcruz <atcruz1@up.edu.ph>
2016-06-22 09:48:33 +0800

    Changed config.php once again

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/itcbs/';
+$config['base_url'] = 'http://localhost/CMSC198/';
 
 /*
 |--------------------------------------------------------------------------
