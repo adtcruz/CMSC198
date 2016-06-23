ed8c2a570341a56883222ca68322c57cad322288
adtcruz <atcruz1@up.edu.ph>
2016-06-23 09:51:47 +0800

    Adds assigned job to schedule

--- application/controllers/Assign_technician_controller.php
+++ application/controllers/Assign_technician_controller.php
@@ -15,8 +15,14 @@ class Assign_technician_controller extends CI_Controller
   					//loads Code Igniter database module
   					$this->load->database();
 
+						if($_SESSION["type"]==="admin") $adminID = $this->db->query("SELECT adminID FROM adminAcc WHERE username='".$_SESSION["username"]."'")->result_array()[0]["adminID"];
+						if($_SESSION["type"]==="superadmin") $adminID = $this->db->query("SELECT superAdminID FROM superAdmin WHERE username='".$_SESSION["username"]."'")->result_array()[0]["superAdminID"];
+
 						//sets job's adminID value from technician ID
   					$this->db->query("UPDATE job SET adminID=".$_POST["technicianID"].", jobStatus='PROCESSING' WHERE jobID=".$_POST["jobID"]."");
+
+						//inserts job to schedule
+						$this->db->query("INSERT INTO schedule(jobID,dateCreated,createdBy,createdByType) VALUES (".$_POST["jobID"].",CURDATE(),".$adminID.",'admin')");
 
 						if(count($this->db->query("SELECT jobID, adminID FROM job WHERE adminID=".$_POST["technicianID"]." AND jobID=".$_POST["jobID"]."")->result_array())==1){
 							echo "Assigned";
