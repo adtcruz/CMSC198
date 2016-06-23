47ed97583a28dbceefd15dca5ee1e7995ceda149
adtcruz <atcruz1@up.edu.ph>
2016-06-23 10:43:08 +0800

    Adjusted designs

--- application/views/GenerateBill_view.php
+++ application/views/GenerateBill_view.php
@@ -4,19 +4,18 @@
 	<div id="navarea" class="col s3 m3 l3 section"><br/><br/></div>
 	<div id="mainc" class="col s9 m9 l9 section">
 		<div class="row">
-			<div class="col s1 m1 l1" background>&nbsp;</div>
-			<form autocomplete="off" class="col s10 m10 l10">
+			<form autocomplete="off">
 				<br/>
-				<h3>Generate Bill</h3>
+				<h3 class="center-align">Generate Bill</h3>
 				<br/>
 					<?php
 					$this->load->library ('table');
 					$template = array (
-							'table_open' => '<table class="bordered">'
+							'table_open' => '<table class="bordered centered highlight">'
 						);
 					$this->table->set_template ($template);
 					$this->table->set_heading ('Job ID', 'Client Name', 'Generate Bill');
-					
+
 					$button = array(
 							'data' => '<a href="topdf_bfp" class="waves-effect waves-light btn"><i class="material-icons left">credit_card</i>Generate</a>'
 						);
@@ -24,15 +23,14 @@
 					foreach ($generateBill_array as $row)
 					{
 						$this->table->add_row ($row['jobID'], $row['givenName'].' '.$row['lastName'], $button);
-						
+
 					}
-					
+
 					echo $this->table->generate ();
 				?>
 				<br/>
 				<br/>
 			</form>
-			<div class="col s1 m1 l1">&nbsp;</div>
 		</div>
 	</div>
 </div>
--- application/views/Schedule_view.php
+++ application/views/Schedule_view.php
@@ -4,22 +4,24 @@
 	<div id="navarea" class="col s3 m3 l3 section"><br/><br/></div>
 	<div id="mainc" class="col s9 m9 l9 section">
 		<div class="row">
-			<div class="col s1 m1 l1" background>&nbsp;</div>
-			<form autocomplete="off" class="col s10 m10 l10">
+			<form autocomplete="off">
 				<br/>
 				<h3 class="center-align">Schedule</h3>
 				<br/>
 					<?php
 					$this->load->library ('table');
 					$template = array (
-							'table_open' => '<table class="bordered">'
+							'table_open' => '<table class="bordered centered highlight">'
 						);
 					$this->table->set_template ($template);
-					$this->table->set_heading ('Priority', 'Date Scheduled', 'Job Description', 'Client Name', 'Location');
+					$this->table->set_heading ('Priority', 'Date Scheduled', 'Job Description', 'Client Name', 'Location', 'Action');
 
 					foreach ($schedule_array as $row)
 					{
-						$this->table->add_row ($row['priority'], $row['dateCreated'], $row['jobDescription'], $row['givenName'].' '.$row['lastName'], $row['officeName']);
+						if($row['priority']==1) $priorty = "Normal";
+						if($row['priority']==2) $priorty = "Urgent";
+						if($row['priority']==3) $priorty = "Very Urgent";
+						$this->table->add_row ($priorty, $row['dateCreated'], $row['jobDescription'], $row['givenName'].' '.$row['lastName'], $row['officeName'],"");
 					}
 
 					echo $this->table->generate ();
@@ -27,7 +29,6 @@
 				<br/>
 				<br/>
 			</form>
-			<div class="col s1 m1 l1">&nbsp;</div>
 		</div>
 	</div>
 </div>
