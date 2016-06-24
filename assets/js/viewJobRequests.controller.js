dcab6b68fb14ac911b1c33e6a8bb71e56cd82e0a
adtcruz <atcruz1@up.edu.ph>
2016-06-24 16:03:33 +0800

    Added a fair bit of documentation

--- assets/js/viewJobRequests.controller.js
+++ assets/js/viewJobRequests.controller.js
@@ -1,18 +1,34 @@
 $('document').ready(
 	function(){
+		//adds black background colour to the view job requests / my job requests button
+		//this adds a "selected" effect
 		$("#viewJRButton").addClass("black");
 	}
 );
 
+//job ID of the selected job
 job_ID = 0;
+//new job description set to blank
 new_job_description = "";
+//old job description set to blank
 old_job_description = "";
 
+//triggered when the cancel job button is clicked
+//opens the modal asking if the user wants to cancel the job or not
+//modal can't be closed by clicking outside it
+//it has to click the no button in order to close it
 function confirmCancel(jobID){
 	$("#cancelModal").openModal({dismissible:false});
-	job_ID = jobID;
+	job_ID = jobID; //sets the job_ID from the jobID of the selected job
 }
 
+//triggered when the "Yes" button in the modal confirming user action is clicked
+//function that handles cancelling the jobID
+//closes the cancel job modal then communicates to the server via jQuery POST
+//the job ID of the selected job is sent as post argument
+//opens the modal containing "Job canceled" message
+//in the case it gets a "Job canceled" message from the server
+//the message modal can only be closed by clicking OK
 function cancelJob(url){
 	$("#cancelModal").closeModal();
 	$.post(url+"cancel_job",{jobID:job_ID},function(data){
@@ -22,10 +38,14 @@ function cancelJob(url){
 	});
 }
 
+//reloads the page
 function reloadPage(url){
 	window.location.href = url+"view_jobs";
 }
 
+//triggered when the "add to schedule" button is clicked
+//rewrites the main app area containing the Job Requests main label and table
+//with a form asking the user for the date of the schedule
 function openAddToSched(url,jobID){
 	$('.tooltipped').tooltip('remove');
 	$.get(url+"get_add_to_schedule_form",function(data){
@@ -36,7 +56,7 @@ function openAddToSched(url,jobID){
 			format: 'yyyy-mm-dd'
 		});
 	});
-	job_ID = jobID;
+	job_ID = jobID; //sets the job_ID from the jobID of the selected job
 }
 
 function addToSchedule(url){
