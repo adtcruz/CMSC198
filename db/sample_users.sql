b619fc17b6cfabadeaa56701fc5aeb77390e28e0
adtcruz <atcruz1@up.edu.ph>
2016-06-24 09:01:49 +0800

    Changed sample admin and technician usernames, added sample superadmin

--- db/sample_users.sql
+++ db/sample_users.sql
@@ -1,13 +1,16 @@
 -- use database created in development_schema.sql
 USE cmsc198db;
 
--- insert sample technicians
+-- insert sample admin and technician account
 INSERT INTO adminAcc (username, password, givenName, lastName, isTechnician, active) VALUES
-('user1',SHA1('useruser'),'User','User1',0,1),
-('user2',SHA1('useruser'),'User','User2',1,1),
-('user3',SHA1('useruser'),'User','User3',1,0);
+('adminUser',SHA1('useruser'),'Admin','User',0,1),
+('technicianUser',SHA1('useruser'),'Technician','User',1,1);
 
 -- insert sample clients
 INSERT INTO client (username, password, givenName, lastName, designation, officeId) VALUES
 ('adtcruz', SHA1('password'), 'Anton', 'Cruz', 'On-the-Job Trainee', 46),
 ('clstia', SHA1('clstia'), 'Ellis', 'Eurolfan', 'On-the-Job Trainee', 46);
+
+-- insert sample superadmin account
+INSERT INTO superAdmin (username, password, givenName, lastName) VALUES
+('superAdmin',SHA1('superadmin'),'Super','Admin');
