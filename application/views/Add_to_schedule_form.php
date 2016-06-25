268e24e6f9d27ad7e0b1c7d3277e2a4e51e5b04b
adtcruz <atcruz1@up.edu.ph>
2016-06-25 15:33:54 +0800

    Added provision for job priority and design improvements

--- application/views/Add_to_schedule_form.php
+++ application/views/Add_to_schedule_form.php
@@ -4,7 +4,20 @@
     <h5 class="center-align">Add the selected job to schedule on the following date?</h5>
     <br/>
     <div class="col s4 m4 l4">&nbsp;</div>
-    <input id="scheduleDate" type="date" class="center-align datepicker col s4 m4 l4"/>
+    <div class="col s4 m4 l4">
+      <div class="input-field">
+        <input id="scheduleDate" name="scheduleDate" type="date" class="center-align datepicker"/>
+        <label for="scheduleDate"><i class="material-icons prefix">today</i></label>
+      </div>
+      <div class"input-field">
+        <select id="jobPrioritySelect">
+          <option value="" disabled="true" selected="true">Select priority…</option>
+          <option value="1">Normal</option>
+          <option value="2">Urgent</option>
+          <option value="1">Very Urgent</option>
+        </select>
+      </div>
+    </div>
     <div class="col s4 m4 l4">&nbsp;</div>
   </div>
   <div class="row center-align">
