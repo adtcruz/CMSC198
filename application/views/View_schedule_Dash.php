0852d1c53027f15161f628d61d91ac0c683676c1
adtcruz <atcruz1@up.edu.ph>
2016-06-25 17:10:57 +0800

    Removed unnecessary form block

--- application/views/View_schedule_Dash.php
+++ application/views/View_schedule_Dash.php
@@ -4,36 +4,34 @@
 	<div id="navarea" class="col s3 m3 l3 section"><br/><br/></div>
 	<div id="mainc" class="col s9 m9 l9 section">
 		<div class="row">
-			<form autocomplete="off">
-				<br/>
-				<h3 class="center-align">Schedule</h3>
-				<br/>
-					<?php
-					$this->load->library ('table');
-					$template = array (
-							'table_open' => '<table class="bordered centered highlight">'
-						);
-					$this->table->set_template ($template);
-					$this->table->set_heading ('Priority', 'Date Scheduled', 'Job Description', 'Client Name', 'Location', 'Action');
+			<br/>
+			<h3 class="center-align">Schedule</h3>
+			<br/>
+				<?php
+				$this->load->library ('table');
+				$template = array (
+						'table_open' => '<table class="bordered centered highlight">'
+					);
+				$this->table->set_template ($template);
+				$this->table->set_heading ('Priority', 'Date Scheduled', 'Job Description', 'Client Name', 'Location', 'Action');
 
-					if(count($schedule_array) == 0){
-						echo "<h5 class=\"center-align\">Sorry, there are no scheduled job requests at the moment.</h5>";
-						return;
-					}
+				if(count($schedule_array) == 0){
+					echo "<h5 class=\"center-align\">Sorry, there are no scheduled job requests at the moment.</h5>";
+					return;
+				}
 
-					foreach ($schedule_array as $row)
-					{
-						if($row['priority']==1) $priority = "Normal";
-						if($row['priority']==2) $priority = "Urgent";
-						if($row['priority']==3) $priority = "Very Urgent";
-						$this->table->add_row ($priority, $row['dateCreated'], $row['jobDescription'], $row['givenName'].' '.$row['lastName'], $row['officeName'],"");
-					}
+				foreach ($schedule_array as $row)
+				{
+					if($row['priority']==1) $priority = "Normal";
+					if($row['priority']==2) $priority = "Urgent";
+					if($row['priority']==3) $priority = "Very Urgent";
+					$this->table->add_row ($priority, $row['dateCreated'], $row['jobDescription'], $row['givenName'].' '.$row['lastName'], $row['officeName'],"");
+				}
 
-					echo $this->table->generate ();
-				?>
-				<br/>
-				<br/>
-			</form>
+				echo $this->table->generate ();
+			?>
+			<br/>
+			<br/>
 		</div>
 	</div>
 </div>
