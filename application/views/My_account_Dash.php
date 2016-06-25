c74e0dc2a5c4dd3056de8579836763ea6bcbc439
adtcruz <atcruz1@up.edu.ph>
2016-06-25 18:05:38 +0800

    Added fields to change user given name or last name

--- application/views/My_account_Dash.php
+++ application/views/My_account_Dash.php
@@ -7,5 +7,37 @@
 		<br/>
 		<h3 class="center-align">My Account</h3>
 		<br/>
+		<div class="row">
+			<div class="col s1 m1 l1">&nbsp;</div>
+			<div class="col s10 m10 l10">
+				<div class="input-field">
+					<i class="material-icons prefix">perm_identity</i>
+					<input name="username" type="text" value="<?php echo $_SESSION["username"];?>" disabled="disabled"/>
+					<label for="username">Username: </label>
+				</div>
+				<div class="input-field">
+					<i class="material-icons prefix">recent_actors</i>
+					<input name="userType" type="text" value="<?php echo $_SESSION["type"];?>" disabled="disabled"/>
+					<label for="userType">Account type: </label>
+				</div>
+				<div class="input-field">
+					<i class="material-icons prefix">contacts</i>
+					<input id="user_givenName" name="user_givenName" type="text" value="<?php echo $_SESSION["givenName"];?>"/>
+					<label for="user_givenName">Given Name: </label>
+				</div>
+				<div class="input-field">
+					<i class="material-icons prefix">contacts</i>
+					<input id="user_lastName" name="user_lastName" type="text" value="<?php echo $_SESSION["lastName"];?>"/>
+					<label for="user_lastName">Last Name: </label>
+				</div>
+				<br/><br/>
+				<div class="center-align">
+					<a class="waves-effect waves-light btn green darken-4 center-align white-text">Update Profile</a>
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<a class="waves-effect waves-light btn blue darken-4 center-align white-text">Change Password…</a>
+				</div>
+			</div>
+			<div class="col s1 m1 l1">&nbsp;</div>
+		</div>
 	</div>
 </div>
