8b46ecf349c01cbe718c36b769b84c5a6946c81c
adtcruz <atcruz1@up.edu.ph>
2016-06-25 16:51:28 +0800

    Modified to link to new routes

--- application/views/Admin_menu.php
+++ application/views/Admin_menu.php
@@ -1,4 +1,4 @@
 <li id="fileJRButton"><a id="fileJRItem" class="waves-effect waves-light white-text" href="<?php base_url();?>new_request">File Job Request</a></li>
 <li id="viewJRButton"><a id="viewJRItem" class="waves-effect waves-light white-text" href="<?php base_url();?>view_jobs">View Job Requests</a></li>
-<li id="viewSchedButton"><a id="viewSchedItem" class="waves-effect waves-light white-text" href="<?php base_url();?>schedule">View Schedule</a></li>
+<li id="viewSchedButton"><a id="viewSchedItem" class="waves-effect waves-light white-text" href="<?php base_url();?>view_schedule">View Schedule</a></li>
 <li id="genBillButton"><a id="gnrtBlItem" class="waves-effect waves-light white-text" href="<?php base_url();?>generate_bill">Generate Bill</a></li>
