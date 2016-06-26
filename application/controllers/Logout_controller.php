b6b4f4338e053d13679551d2cee93ab8b400134a
adtcruz <atcruz1@up.edu.ph>
2016-06-26 15:22:32 +0800

    Renamed login and logout backend controller files

--- application/controllers/Logout_controller.php
+++ application/controllers/Logout_controller.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Logout extends CI_Controller {
+class Logout_controller extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
