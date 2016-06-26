21e4edf4204c47e017354a8e2c66cb55adcb64af
adtcruz <atcruz1@up.edu.ph>
2016-06-26 14:08:00 +0800

    Viewing of job requests now using properly-working model

--- application/controllers/View_job_requests_controller.php
+++ application/controllers/View_job_requests_controller.php
@@ -12,29 +12,15 @@ class View_job_requests_controller extends CI_Controller
 
 			$this->table->set_template(array('table_open' =>'<table class="bordered centered highlight responsive-table">'));
 
+			$this->load->model('View_job_requests_model','vjrm');
+
 			if($_SESSION["type"] === "client"){
 
-				//loads Code Igniter database module
-				$this->load->database();
-
-				//table heading
-				$this->table->set_heading("Description","Start Date","Finish Date","Status","Date Filed","Actions");
-
-				//queries the database for the client ID of the user in session
-				$query = $this->db->query("SELECT clientID FROM client WHERE username='".$_SESSION["username"]."'");
-
-				//gets the results in easy-to-use array form
-				$rows = $query->result_array();
 				//gets the client ID
-				$clientID = $rows[0]["clientID"];
+				$clientID = $this->vjrm->getUserID();
 
 				//queries the DB
-				$query1 = $this->db->query(
-					"SELECT jobID, jobDescription, startDate, finishDate, jobStatus, adminID, dateCreated FROM job WHERE clientID=".$clientID." ORDER BY jobID DESC"
-				);
-
-				//gets the results in easy-to-use array form
-				$rows1 = $query1->result_array();
+				$rows1 = $this->vjrm->getJobRequestsArray(" WHERE clientID=".$clientID." ");
 
 				$nRows1 = count($rows1);
 
@@ -44,40 +30,21 @@ class View_job_requests_controller extends CI_Controller
 					return;
 				}
 
+				//table heading
+				$this->table->set_heading("Description","Start Date","Finish Date","Status","Date Filed","Actions");
+
 				for ($i = 0; $i < $nRows1; $i++){
 
-					$startDate = "";
-					//start date column entry
-					if($rows1[$i]["startDate"] !== NULL){
-						$startDate = date("F d, Y", strtotime($rows1[$i]["startDate"]));
-					}
+					$startDate = $this->vjrm->processDate($rows1[$i]["startDate"]);
 
-					$finishDate = "";
-					//finish date column entry
-					if($rows1[$i]["finishDate"] !== NULL){
-						$finishDate = date("F d, Y", strtotime($rows1[$i]["finishDate"]));
-					}
+					$finishDate = $this->vjrm->processDate($rows1[$i]["finishDate"]);
 
-					$jobStatus = "";
-					$actions = "";
-					//job status column entry
-					if($rows1[$i]["jobStatus"] === "PENDING"){
-						$jobStatus = "<span class=\"blue-text\">".$rows1[$i]["jobStatus"]."</span>";
-						$actions = "<a class=\"btn-floating btn tooltipped waves-effect waves-light cyan\" data-position=\"left\" data-delay=\"50\" data-tooltip=\"Edit Job Request\" onclick=\"openEditModal(".$rows1[$i]["jobID"].");\"><i class=\"material-icons\">mode_edit</i></a>&nbsp;&nbsp;&nbsp;&nbsp;";
-						$actions = $actions . "<a class=\"btn-floating btn tooltipped waves-effect waves-light red\" data-position=\"left\" data-delay=\"50\" data-tooltip=\"Cancel Job Request\" onclick=\"confirmCancel(".$rows1[$i]["jobID"].");\"><i class=\"material-icons\">not_interested</i></a>";
-					}
-					else if($rows1[$i]["jobStatus"] === "PROCESSING"){
-						$jobStatus = "<span class=\"orange-text\">".$rows1[$i]["jobStatus"]."</span>";
-					}
-					else if($rows1[$i]["jobStatus"] === "CANCELED"){
-						$jobStatus = "<span class=\"red-text\">".$rows1[$i]["jobStatus"]."</span>";
-					}
-					else if($rows1[$i]["jobStatus"] === "PROCESSED"){
-						$jobStatus = "<span class=\"green-text\">".$rows1[$i]["jobStatus"]."</span>";
-					}
+					$jobStatus = $this->vjrm->processJobStatus($rows1[$i]["jobStatus"]);
+
+					$actions = $this->vjrm->getJobActions($rows1[$i],$clientID);
 
 					//date filed column entry
-					$dateFiled = date("F d, Y", strtotime($rows1[$i]["dateCreated"]));
+					$dateFiled = $this->vjrm->processDate($rows1[$i]["dateCreated"]);
 
 					$this->table->add_row($rows1[$i]["jobDescription"],$startDate,$finishDate,$jobStatus,$dateFiled,$actions);
 				}
@@ -89,10 +56,7 @@ class View_job_requests_controller extends CI_Controller
 				//loads Code Igniter database module
 				$this->load->database();
 
-				$this->table->set_heading('Description','Client','Start Date','Finish Date','Status','Date Filed','Filed By','Actions');
-
-				$query1 = $this->db->query("SELECT jobID, jobDescription, startDate, finishDate, jobStatus, clientID, adminID, dateCreated, createdBy, createdByType FROM job ORDER BY dateCreated DESC, jobID DESC");
-				$rows1 = $query1->result_array();
+				$rows1 = $this->vjrm->getJobRequestsArray(" ");
 				$nRows1 = count($rows1);
 
 				if($nRows1 == 0){
@@ -101,69 +65,26 @@ class View_job_requests_controller extends CI_Controller
 					return;
 				}
 
+				$this->table->set_heading('Description','Client','Start Date','Finish Date','Status','Date Filed','Filed By','Actions');
+
 				for ($i = 0; $i < $nRows1; $i++){
 
-					$query2 = $this->db->query("SELECT givenName, lastName FROM client WHERE clientID=".$rows1[$i]["clientID"]."");
-					$rows2 = $query2->result_array();
+					$userID = $this->vjrm->getUserID();
 
-					$clientName = $rows2[0]["givenName"]." ".$rows2[0]["lastName"];
+					$clientName = $this->vjrm->getClientName($rows1[$i]["clientID"]);
 
-					$startDate = "";
-					if($rows1[$i]["startDate"] !== NULL){
-						$startDate = date("F d, Y", strtotime($rows1[$i]["startDate"]));
-					}
+					$startDate = $this->vjrm->processDate($rows1[$i]["startDate"]);
 
-					$finishDate = "";
-					if($rows1[$i]["finishDate"] !== NULL){
-						$finishDate = date("F d, Y", strtotime($rows1[$i]["finishDate"]));
-					}
+					$finishDate = $this->vjrm->processDate($rows1[$i]["finishDate"]);
 
-					$jobStatus = "";
-					$actions = "";
-					if($rows1[$i]["jobStatus"] === "PENDING"){
-						$jobStatus = "<span class=\"blue-text\">".$rows1[$i]["jobStatus"]."</span>";
-						if(($_SESSION["type"]==="technician")||($_SESSION["type"]==="admin")||($_SESSION["type"]==="superadmin")) $actions = "<a class=\"btn-floating btn tooltipped waves-effect waves-light green\" data-position=\"left\" data-delay=\"50\" data-tooltip=\"Add job to schedule\" onclick=\"openAddToSched('".base_url()."',".$rows1[$i]["jobID"].");\"><i class=\"material-icons\">assignment</i></a>&nbsp;&nbsp;";
-					}
-					else if($rows1[$i]["jobStatus"] === "PROCESSING"){
-						$jobStatus = "<span class=\"orange-text\">".$rows1[$i]["jobStatus"]."</span>";
-					}
-					else if($rows1[$i]["jobStatus"] === "CANCELED"){
-						$jobStatus = "<span class=\"red-text\">".$rows1[$i]["jobStatus"]."</span>";
-					}
-					else if($rows1[$i]["jobStatus"] === "PROCESSED"){
-						$jobStatus = "<span class=\"green-text\">".$rows1[$i]["jobStatus"]."</span>";
-					}
+					$jobStatus = $this->vjrm->processJobStatus($rows1[$i]["jobStatus"]);
 
-					$dateFiled = date("F d, Y", strtotime($rows1[$i]["dateCreated"]));
+					$actions = $this->vjrm->getJobActions($rows1[$i],$userID);
 
-					$filedBy = "";
+					//date filed column entry
+					$dateFiled = $this->vjrm->processDate($rows1[$i]["dateCreated"]);
 
-					if ($rows1[$i]["createdByType"] === "client"){
-						$query2 = $this->db->query("SELECT givenName, lastName FROM client WHERE clientID=".$rows1[$i]["clientID"]."");
-						$rows2 = $query2->result_array();
-
-						$filedBy = $rows2[0]["givenName"]." ".$rows2[0]["lastName"];
-					}
-					else if (($rows1[$i]["createdByType"] === "technician")||($rows1[$i]["createdByType"] === "admin")){
-						$query2 = $this->db->query("SELECT givenName, lastName FROM adminAcc WHERE adminID=".$rows1[$i]["createdBy"]."");
-						$rows2 = $query2->result_array();
-
-						$filedBy = $rows2[0]["givenName"]." ".$rows2[0]["lastName"];
-
-						$query2 = $this->db->query("SELECT adminID FROM adminAcc WHERE username='".$_SESSION["username"]."'");
-						$rows2 = $query2->result_array();
-
-						if(($rows1[$i]["createdBy"] == $rows2[0]["adminID"])&&($rows1[$i]["jobStatus"] == "PENDING")){
-							$actions = $actions . "<a class=\"btn-floating btn tooltipped waves-effect waves-light cyan\" data-position=\"left\" data-delay=\"50\" data-tooltip=\"Edit Job Request\" onclick=\"openEditModal(".$rows1[$i]["jobID"].")\"><i class=\"material-icons\">mode_edit</i></a>&nbsp;&nbsp;";
-							$actions = $actions . "<a class=\"btn-floating btn tooltipped waves-effect waves-light red\" data-position=\"left\" data-delay=\"50\" data-tooltip=\"Cancel Job Request\" onclick=\"confirmCancel(".$rows1[$i]["jobID"].");\"><i class=\"material-icons\">not_interested</i></a>";
-						}
-					}
-					else if ($rows1[$i]["createdByType"] === "superadmin"){
-						$query2 = $this->db->query("SELECT givenName, lastName FROM superAdmin WHERE superAdminID=".$rows1[$i]["createdBy"]."");
-						$rows2 = $query2->result_array();
-
-						$filedBy = $rows2[0]["givenName"]." ".$rows2[0]["lastName"];
-					}
+					$filedBy = $this->vjrm->getFiledBy($rows1[$i]);
 
 					$this->table->add_row($rows1[$i]["jobDescription"],$clientName,$startDate,$finishDate,$jobStatus,$dateFiled,$filedBy,$actions);
 
@@ -171,7 +92,7 @@ class View_job_requests_controller extends CI_Controller
 			}
 
 			$tabl = $this->table->generate();
-			$this->load->view('View_jobs_view', array('table' => $tabl));
+			$this->load->view('View_job_requests_view', array('table' => $tabl));
 		}
 		else {
 			die("You are not logged-in");
---
+++ application/models/View_job_requests_model.php
@@ -0,0 +1,153 @@
+<?php
+
+class View_job_requests_model extends CI_Model
+{
+
+	public function __construct ()
+	{
+		parent::__construct ();
+		$this->load->database();
+	}
+
+	public function getJobRequestsArray($whereClause)
+	{
+		$query = $this->db->query(
+			"SELECT jobID, jobDescription, startDate, finishDate, jobStatus, ".
+			"clientID, adminID, dateCreated, createdBy, createdByType FROM job".
+			$whereClause."ORDER BY dateCreated DESC, jobID DESC"
+		);
+		return $query->result_array();
+	}
+
+	public function processDate($dateToProcess)
+	{
+		if($dateToProcess===NULL){
+			return "";
+		}
+		else{
+			return date("F d, Y", strtotime($dateToProcess));
+		}
+	}
+
+	public function getUserID()
+	{
+		if($_SESSION["type"]==="superadmin"){
+			$query = $this->db->query(
+				"SELECT superAdminID FROM superAdmin WHERE username='".$_SESSION["username"]."'"
+			);
+			return $query->result_array()[0]["superAdminID"];
+		}
+		if(($_SESSION["type"]==="admin")||($_SESSION["type"]==="technician")){
+			$query = $this->db->query(
+				"SELECT adminID FROM adminAcc WHERE username='".$_SESSION["username"]."'"
+			);
+			return $query->result_array()[0]["adminID"];
+		}
+		if($_SESSION["type"]==="client"){
+			$query = $this->db->query(
+				"SELECT clientID FROM client WHERE username='".$_SESSION["username"]."'"
+			);
+			return $query->result_array()[0]["clientID"];
+		}
+	}
+
+	public function getClientName($clientID)
+	{
+		$query = $this->db->query(
+			"SELECT givenName, lastName FROM client WHERE clientID=".$clientID.""
+		);
+		$rows = $query->result_array();
+
+		$clientName = $rows[0]["givenName"]." ".$rows[0]["lastName"];
+	}
+
+	public function getFiledBy($currentRow)
+	{
+		if ($currentRow["createdByType"] === "client"){
+			$query = $this->db->query("SELECT givenName, lastName FROM client WHERE clientID=".$currentRow["clientID"]."");
+			$rows = $query->result_array();
+
+			return $rows[0]["givenName"]." ".$rows[0]["lastName"];
+		}
+		else if (($currentRow["createdByType"] === "technician")||($currentRow["createdByType"] === "admin")){
+			$query = $this->db->query("SELECT givenName, lastName FROM adminAcc WHERE adminID=".$currentRow["createdBy"]."");
+			$rows = $query->result_array();
+
+			return $rows[0]["givenName"]." ".$rows[0]["lastName"];
+		}
+		else if ($currentRow["createdByType"] === "superadmin"){
+			$query = $this->db->query("SELECT givenName, lastName FROM superAdmin WHERE superAdminID=".$currentRow["createdBy"]."");
+			$rows = $query->result_array();
+
+			return $rows[0]["givenName"]." ".$rows[0]["lastName"];
+		}
+	}
+
+	public function processJobStatus($jobStatus)
+	{
+		if($jobStatus === "PENDING"){
+			return "<span class=\"blue-text\">".$jobStatus."</span>";
+		}
+		else if($jobStatus === "PROCESSING"){
+			return "<span class=\"orange-text\">".$jobStatus."</span>";
+		}
+		else if($jobStatus === "CANCELED"){
+			return "<span class=\"red-text\">".$jobStatus."</span>";
+		}
+		else if($jobStatus === "PROCESSED"){
+			return "<span class=\"green-text\">".$jobStatus."</span>";
+		}
+	}
+
+	public function getJobActions($currentRow,$userID)
+	{
+		$jobID = $currentRow["jobID"];
+		$jobStatus = $currentRow["jobStatus"];
+		$createdBy = $currentRow["createdBy"];
+		$createdByType = $currentRow["createdByType"];
+
+		if($jobStatus === "PENDING"){
+
+			$actions = "";
+
+			//superadmins may schedule, edit, or cancel job requests
+			if($_SESSION["type"]==="superadmin"){
+				$actions = "<a class=\"btn-floating btn tooltipped waves-effect waves-light green\" data-position=\"left\" data-delay=\"50\" data-tooltip=\"Add job to schedule\" onclick=\"openAddToSched('".base_url()."',".$jobID.");\"><i class=\"material-icons\">assignment</i></a>&nbsp;&nbsp;";
+				$actions = $actions . "<a class=\"btn-floating btn tooltipped waves-effect waves-light cyan\" data-position=\"left\" data-delay=\"50\" data-tooltip=\"Edit Job Request\" onclick=\"openEditModal(".$jobID.");\"><i class=\"material-icons\">mode_edit</i></a>&nbsp;&nbsp;&nbsp;&nbsp;";
+				return $actions . "<a class=\"btn-floating btn tooltipped waves-effect waves-light red\" data-position=\"left\" data-delay=\"50\" data-tooltip=\"Cancel Job Request\" onclick=\"confirmCancel(".$jobID.");\"><i class=\"material-icons\">not_interested</i></a>";
+			}
+
+			//admins and technicians can schedule job requests
+			if(($_SESSION["type"]==="admin")||($_SESSION["type"]==="technician")){
+				$actions = "<a class=\"btn-floating btn tooltipped waves-effect waves-light green\" data-position=\"left\" data-delay=\"50\" data-tooltip=\"Add job to schedule\" onclick=\"openAddToSched('".base_url()."',".$jobID.");\"><i class=\"material-icons\">assignment</i></a>&nbsp;&nbsp;";
+			}
+
+			//users can only edit or cancel job requests that they filed
+			if($_SESSION["type"]===$createdByType){
+				if($userID == $createdBy){
+					$actions = $actions . "<a class=\"btn-floating btn tooltipped waves-effect waves-light cyan\" data-position=\"left\" data-delay=\"50\" data-tooltip=\"Edit Job Request\" onclick=\"openEditModal(".$jobID.");\"><i class=\"material-icons\">mode_edit</i></a>&nbsp;&nbsp;&nbsp;&nbsp;";
+					return $actions . "<a class=\"btn-floating btn tooltipped waves-effect waves-light red\" data-position=\"left\" data-delay=\"50\" data-tooltip=\"Cancel Job Request\" onclick=\"confirmCancel(".$jobID.");\"><i class=\"material-icons\">not_interested</i></a>";
+				}
+			}
+		}
+		else if($jobStatus === "PROCESSING"){
+
+			$actions = "";
+
+			//superadmins may edit or cancel job requests that are processing
+			if($_SESSION["type"]==="superadmin"){
+				$actions = $actions . "<a class=\"btn-floating btn tooltipped waves-effect waves-light cyan\" data-position=\"left\" data-delay=\"50\" data-tooltip=\"Edit Job Request\" onclick=\"openEditModal(".$jobID.");\"><i class=\"material-icons\">mode_edit</i></a>&nbsp;&nbsp;&nbsp;&nbsp;";
+				return $actions . "<a class=\"btn-floating btn tooltipped waves-effect waves-light red\" data-position=\"left\" data-delay=\"50\" data-tooltip=\"Cancel Job Request\" onclick=\"confirmCancel(".$jobID.");\"><i class=\"material-icons\">not_interested</i></a>";
+			}
+		}
+		//no actions can be made if job request was already processed or cancelled
+		else if($jobStatus === "CANCELED"){
+			return "";
+		}
+		else if($jobStatus === "PROCESSED"){
+			return "";
+		}
+	}
+
+}
+?>
