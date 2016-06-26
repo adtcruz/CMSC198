e25a37e1fb27eb3022bf8c2f140fc8b80174f097
adtcruz <atcruz1@up.edu.ph>
2016-06-26 14:30:35 +0800

    Properly renamed script file for convention

--- application/views/View_job_requests_script.php
+++ application/views/View_job_requests_script.php
@@ -1,1 +1,1 @@
-<script type="text/javascript" src="<?php base_url();?>assets/js/View_jobs.controller.js"></script>
+<script type="text/javascript" src="<?php base_url();?>assets/js/View_job_requests.controller.js"></script>
