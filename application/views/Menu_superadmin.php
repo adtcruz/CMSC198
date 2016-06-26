76d6faef6e3d169d87568f453463f168dd8c3e6b
adtcruz <atcruz1@up.edu.ph>
2016-06-26 16:55:44 +0800

    Added link to view user logs page

--- application/views/Menu_superadmin.php
+++ application/views/Menu_superadmin.php
@@ -1,2 +1,2 @@
-<li id="viewULButton"><a id="viewULItem" class="waves-effect waves-light white-text">View User Logs</a></li>
+<li id="viewULButton"><a id="viewULItem" class="waves-effect waves-light white-text" href="<?php echo base_url();?>view_logs">View User Logs</a></li>
 <li id="mnageUButton"><a id="mnageUItem" class="waves-effect waves-light white-text">Manage Users</a></li>
