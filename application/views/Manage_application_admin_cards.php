46cfc1a8c71a9d5680d6a3e5acdcc28d136efe88
adtcruz <atcruz1@up.edu.ph>
2016-06-27 18:52:28 +0800

    Added viewing of numbers of technician and admin accounts

--- application/views/Manage_application_admin_cards.php
+++ application/views/Manage_application_admin_cards.php
@@ -17,6 +17,36 @@
     <div class="card grey lighten-2">
       <div class="card-content black-text">
         <span class="card-title center-align">Admin and Technician Accounts</span>
+        <br/><br/><span>Currently:</span>
+        <ul>
+          <li>
+            <?php
+            if ($nAdmin==0){
+              echo "No Admin Accounts";
+            }
+            if ($nAdmin==1){
+              echo "1 Admin Account";
+            }
+            else{
+              echo $nAdmin . " Admin Accounts";
+            }
+            ?>
+          </li>
+          <li>
+            <?php
+            if ($nTechn==0){
+              echo "No Technician Accounts";
+            }
+            if ($nTechn==1){
+              echo "1 Technician Account";
+            }
+            else{
+              echo $nTechn . " Technician Accounts";
+            }
+            ?>
+          </li>
+        </ul>
+        <br/><br/>
       </div>
       <div class="card-action center-align">
         <a class="blue-text" href="#">Manage Admin and Technician accounts</a>
