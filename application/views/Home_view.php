2561b75bce01458729b78c0e0c77428f78fb1c04
adtcruz <atcruz1@up.edu.ph>
2016-06-28 10:58:20 +0800

    Renamed Dashboard to Home_Dash for convention

--- application/views/Home_view.php
+++ application/views/Home_view.php
@@ -4,14 +4,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $this->load->view('Header');
 
-$this->load->view('Dashboard');
+$this->load->view('Home_Dash');
 
 $this->load->view('Common_scripts');
 
 $this->load->view('Home_script');
 
 $this->load->view('Logout_script');
-
 ?>
 	</body>
 </html>
