75ee48973c4cae2fbffce5a59f57124d90c77720
adtcruz <atcruz1@up.edu.ph>
2016-06-28 18:16:57 +0800

    Separate New Admin Accounts and New Technician Accounts tabs

--- application/views/New_account_superadmin_tabs.php
+++ application/views/New_account_superadmin_tabs.php
@@ -1,7 +1,9 @@
 <div class="col s12">
   <ul class="tabs">
-    <li class="tab col s3"><a class="active" href="#newClientAccount">New Client Account</a></li>
-    <li class="tab col s3"><a href="#newAdminTechAccount" onclick="$('select').material_select();">New Admin or Techinician Account</a></li>
+    <li class="tab col s2"><a class="active" href="#newClientAccount">New Client Account</a></li>
+    <li class="tab col s2"><a href="#newAdminAccount">New Admin Account</a></li>
+    <li class="tab col s2"><a href="#newTechnAccount">New Technician Account</a></li>
+    <li class="tab col s2"><a href="#newSuperadminAccount">New Superadmin Account</a></li>
   </ul>
   <br/>
 </div>
