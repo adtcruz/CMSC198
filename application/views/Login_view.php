3ade2c128318aa7017ebd86e6e082722238eb090
adtcruz <atcruz1@up.edu.ph>
2016-06-28 10:31:17 +0800

    Made a separate view page for login screen

---
+++ application/views/Login_view.php
@@ -0,0 +1,15 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$this->load->view('Header');
+
+$this->load->view('Login_page');
+
+$this->load->view('Common_scripts');
+
+$this->load->view('Login_script');
+
+?>
+	</body>
+</html>
