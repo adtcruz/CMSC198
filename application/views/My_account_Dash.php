1844f5bf23da4ceaeea1ebe758ada175cae4b9f7
adtcruz <atcruz1@up.edu.ph>
2016-06-30 20:00:14 +0800

    Added error messages in My account

--- application/views/My_account_Dash.php
+++ application/views/My_account_Dash.php
@@ -20,24 +20,34 @@
 					<input name="userType" type="text" value="<?php echo $_SESSION["type"];?>" disabled="disabled"/>
 					<label for="userType">Account type: </label>
 				</div>
-				<div class="input-field">
-					<i class="material-icons prefix">contacts</i>
-					<input id="user_givenName" name="user_givenName" type="text" value="<?php echo $_SESSION["givenName"];?>"/>
-					<label for="user_givenName">Given Name: </label>
-				</div>
-				<div class="input-field">
-					<i class="material-icons prefix">contacts</i>
-					<input id="user_lastName" name="user_lastName" type="text" value="<?php echo $_SESSION["lastName"];?>"/>
-					<label for="user_lastName">Last Name: </label>
+				<br/>
+				<div class="row">
+					<div class="input-field col s6 m6 l6">
+						<input id="user_givenName" name="user_givenName" type="text" onkeyup="givenNameOnChange();" onchange="givenNameOnChange()" value="<?php echo $_SESSION["givenName"];?>"/>
+						<label id="user_givenNameLabel" for="user_givenName">Given Name: </label>
+					</div>
+					<div class="input-field col s6 m6 l6">
+						<input id="user_lastName" name="user_lastName" type="text" onkeyup="lastNameOnChange();" onchange="lastNameOnChange()" value="<?php echo $_SESSION["lastName"];?>"/>
+						<label id="user_lastNameLabel"for="user_lastName">Last Name: </label>
+					</div>
 				</div>
 				<br/><br/>
 				<div class="center-align">
-					<a class="waves-effect waves-light btn green darken-4 center-align white-text">Update Profile</a>
+					<a class="waves-effect waves-light btn green darken-4 center-align white-text" onclick="updateProfile('<?php echo base_url();?>')">Update Profile</a>
 					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 					<a class="waves-effect waves-light btn blue darken-4 center-align white-text">Change Password…</a>
 				</div>
 			</div>
 			<div class="col s1 m1 l1">&nbsp;</div>
 		</div>
+	</div>
+	<div id="profileUpdatedModal" class="modal">
+		<div class="row">
+	    <br/>
+	    <h5 class="center-align">Profile updated!</h5>
+	  </div>
+	  <div class="row center-align">
+	    <a class="waves-effect waves-light btn btn-large blue darken-4" onclick="$('#profileUpdatedModal').closeModal();">OK</a>
+	  </div>
 	</div>
 </div>
--- assets/js/My_account.controller.js
+++ assets/js/My_account.controller.js
@@ -1,5 +1,50 @@
+user_givenName_old = "";
+user_lastName_old = "";
+
 $('document').ready(
 	function(){
 		$("#myAccountButton").addClass("black");
+		user_givenName_old = $("#user_givenName").val();
+		user_lastName_old = $("#user_lastName").val();
 	}
 );
+
+function givenNameOnChange(){
+	$("#user_givenNameLabel").attr("data-error","Given Name can not be blank!");
+	$("#user_givenNameLabel").html("Given Name: ");
+	if($("#user_givenName").hasClass("invalid")) $("#user_givenName").removeClass("invalid");
+}
+
+function lastNameOnChange(){
+	$("#user_lastNameLabel").attr("data-error","Given Name can not be blank!");
+	$("#user_lastNameLabel").html("Last Name: ");
+	if($("#user_lastName").hasClass("invalid")) $("#user_lastName").removeClass("invalid");
+}
+
+function updateProfile(url){
+
+	if($("#user_givenName").val()===""){
+		$("#user_givenNameLabel").attr("data-error","Given Name can not be blank!");
+		$("#user_givenNameLabel").html(
+			"Given Name:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+
+			"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+
+			"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+		);
+		if(!($("#user_givenName").hasClass("invalid"))) $("#user_givenName").addClass("invalid");
+	}
+
+	if($("#user_lastName").val()===""){
+		$("#user_lastNameLabel").attr("data-error","Last Name can not be blank!");
+		$("#user_lastNameLabel").html(
+			"Last Name:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+
+			"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+
+			"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+		);
+		if(!($("#user_lastName").hasClass("invalid"))) $("#user_lastName").addClass("invalid");
+	}
+
+	if((user_givenName_old===$("#user_givenName").val())||
+		(user_lastName_old = $("#user_lastName").val())){
+
+	}
+}
