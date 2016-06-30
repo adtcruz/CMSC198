c2dee1a700a7e1dcd8e10ac166427622c35a8265
adtcruz <atcruz1@up.edu.ph>
2016-06-30 22:51:15 +0800

    Added modal for changing password

--- application/views/My_account_Dash.php
+++ application/views/My_account_Dash.php
@@ -35,7 +35,7 @@
 				<div class="center-align">
 					<a class="waves-effect waves-light btn green darken-4 center-align white-text" onclick="updateProfile('<?php echo base_url();?>')">Update Profile</a>
 					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					<a class="waves-effect waves-light btn blue darken-4 center-align white-text">Change Password…</a>
+					<a class="waves-effect waves-light btn blue darken-4 center-align white-text" onclick="$('#changePasswordModal').openModal({dismissible:false});">Change Password…</a>
 				</div>
 			</div>
 			<div class="col s1 m1 l1">&nbsp;</div>
@@ -48,6 +48,36 @@
 	  </div>
 	  <div class="row center-align">
 	    <a class="waves-effect waves-light btn btn-large blue darken-4" onclick="$('#profileUpdatedModal').closeModal();">OK</a>
+	  </div>
+	</div>
+	<div id="changePasswordModal" class="modal">
+		<div class="row">
+	    <br/>
+	    <h5 class="center-align">Change Password</h5>
+			<br/>
+			<div id="row">
+				<div class="col s3 m3 l3">&nbsp;</div>
+				<div class="col s6 m6 l6">
+					<div class="input-field">
+						<input id="oldPassword" name="oldPassword" type="password"/>
+						<label id="oldPasswordLabel" for="oldPassword">Re-enter old password</label>
+					</div>
+					<div class="input-field">
+						<input id="newPassword" name="newPassword" type="password"/>
+						<label id="newPasswordLabel" for="newPassword">Enter new password</label>
+					</div>
+					<div class="input-field">
+						<input id="confirmPassword" name="confirmPassword" type="password"/>
+						<label id="confirmPasswordLabel" for="confirmPassword">Confirm new password</label>
+					</div>
+				</div>
+				<div class="col s3 m3 l3">&nbsp;</div>
+			</div>
+	  </div>
+	  <div class="row center-align">
+			<a class="waves-effect waves-light btn btn-large red darken-4">Change Password</a>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+	    <a class="waves-effect waves-light btn btn-large blue darken-4" onclick="$('#changePasswordModal').closeModal();">Cancel</a>
 	  </div>
 	</div>
 </div>
