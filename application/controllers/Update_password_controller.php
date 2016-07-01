9e19f0a985ce020333aad3e66397354793e9c793
adtcruz <atcruz1@up.edu.ph>
2016-07-01 09:55:29 +0800

    Enabled messaging that user did not enter the old password properly

--- application/controllers/Update_password_controller.php
+++ application/controllers/Update_password_controller.php
@@ -19,19 +19,26 @@ class Update_password_controller extends CI_Controller
 
             if($_SESSION["type"]==="client"){
 
-              if(1 == $this->db->query("SELECT COUNT(clientID) FROM client WHERE username='".$_SESSION["username"]."',password=SHA1('".$_POST["passwordOld"]."')")->row_array()["COUNT(clientID)"]){
+              if($this->db->query("SELECT COUNT(clientID) FROM client WHERE username='".$_SESSION["username"]."' AND password=SHA1('".$_POST["passwordOld"]."')")->row_array()["COUNT(clientID)"]){
 
                 $this->db->query("UPDATE client SET password=SHA1('".$_POST["passwordNew"]."') WHERE username='".$_SESSION["username"]."'");
 
                 $this->db->query("INSERT INTO userLogs(logText,logTimestamp) VALUES ('".$_SESSION["username"]." has changed their password',CURRENT_TIMESTAMP)");
 
                 echo "Password updated";
+                return;
+              }
+
+              else{
+
+                echo "Invalid old password";
+                return;
               }
             }
 
             if(($_SESSION["type"]==="admin")||($_SESSION["type"]==="technician")){
 
-              if(1 == $this->db->query("SELECT COUNT(adminID) FROM adminAcc WHERE username='".$_SESSION["username"]."',password=SHA1('".$_POST["passwordOld"]."')")->row_array()["COUNT(adminID)"]){
+              if(1 == $this->db->query("SELECT COUNT(adminID) FROM adminAcc WHERE username='".$_SESSION["username"]."' AND password=SHA1('".$_POST["passwordOld"]."')")->row_array()["COUNT(adminID)"]){
 
                 $this->db->query("UPDATE adminAcc SET password=SHA1('".$_POST["passwordNew"]."') WHERE username='".$_SESSION["username"]."'");
 
@@ -39,17 +46,27 @@ class Update_password_controller extends CI_Controller
 
                 echo "Password updated";
               }
+
+              else{
+
+                echo "Invalid old password";
+              }
             }
 
             if($_SESSION["type"]==="superadmin"){
 
-              if(1 == $this->db->query("SELECT COUNT(superAdminID) FROM superAdmin WHERE username='".$_SESSION["username"]."',password=SHA1('".$_POST["passwordOld"]."')")->row_array()["COUNT(superAdminID)"]){
+              if(1 == $this->db->query("SELECT COUNT(superAdminID) FROM superAdmin WHERE username='".$_SESSION["username"]."' AND password=SHA1('".$_POST["passwordOld"]."')")->row_array()["COUNT(superAdminID)"]){
 
                 $this->db->query("UPDATE superAdmin SET password=SHA1('".$_POST["passwordNew"]."') WHERE username='".$_SESSION["username"]."'");
 
                 $this->db->query("INSERT INTO userLogs(logText,logTimestamp) VALUES ('".$_SESSION["username"]." has changed their given or last name(s)',CURRENT_TIMESTAMP)");
 
                 echo "Password updated";
+              }
+
+              else{
+
+                echo "Invalid old password";
               }
             }
           }
