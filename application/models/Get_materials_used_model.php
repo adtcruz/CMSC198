3a868ea58e8a8ed595bec018d7e85b002c64c5c6
adtcruz <atcruz1@up.edu.ph>
2016-07-02 20:32:26 +0800

    Calls the proper function

--- application/models/Get_materials_used_model.php
+++ application/models/Get_materials_used_model.php
@@ -9,11 +9,12 @@ class Get_materials_used_model extends CI_Model
 
   public function processActions($materialsUsedID){
 		$editOpen = '<a class="btn-floating btn tooltipped waves-effect waves-light cyan" data-position="left" data-delay="50" data-tooltip="Change material quantity/unit"';
+		$editFunctionCall = ' onclick="updateMaterialsUsed(\''.base_url().'\','.$materialsUsedID.')"';
 		$editClose = '><i class="material-icons">mode_edit</i></a>';
 		$space = '&nbsp;&nbsp;&nbsp;&nbsp;';
 		$deleteOpen = '<a class="btn-floating btn tooltipped waves-effect waves-light red" data-position="left" data-delay="50" data-tooltip="Delete material used"';
 		$deleteFunctionCall = ' onclick="deleteMaterialUsed(\''.base_url().'\','.$materialsUsedID.')"';
 		$deleteClose = '><i class="material-icons">not_interested</i></a>';
-		return $editOpen . $editClose . $space . $deleteOpen . $deleteFunctionCall . $deleteClose;
+		return $editOpen . $editFunctionCall . $editClose . $space . $deleteOpen . $deleteFunctionCall . $deleteClose;
   }
 }
