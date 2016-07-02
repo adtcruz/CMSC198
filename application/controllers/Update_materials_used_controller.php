2c86646ec2a9e1eb2ad27c20e34d7234b3640775
adtcruz <atcruz1@up.edu.ph>
2016-07-02 20:18:36 +0800

    Added API for updating materials used quantity/units

---
+++ application/controllers/Update_materials_used_controller.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Update_materials_used_controller extends CI_Controller
+{
+	public function index ()
+	{
+
+		session_start();
+
+		if(array_key_exists("type",$_SESSION)){
+
+			if(($_SESSION["type"]==="superadmin")||($_SESSION["type"]==="technician")){
+
+				if(array_key_exists("materialUsedID",$_POST)){
+
+					if(array_key_exists("materialUnits",$_POST)){
+
+						$this->load->database();
+
+						$this->db->query("UPDATE materialsUsed SET materialUnits=".$_POST["materialUnits"]." WHERE materialsUsedID=".$_POST["materialUsedID"]."");
+
+						$this->db->query("INSERT INTO userLogs(logText,logTimestamp) VALUES ('".$_SESSION["username"]." updated a material for use in jobID #".$_POST["jobID"]."',CURRENT_TIMESTAMP)");
+
+						echo "Added new material";
+
+						return;
+					}
+				}
+			}
+		}
+	}
+}
+?>
