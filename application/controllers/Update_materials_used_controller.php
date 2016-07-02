8e8cbe7d2c0b4f3d3cd32c73260b7a5b1de9ca34
adtcruz <atcruz1@up.edu.ph>
2016-07-02 20:42:16 +0800

    Added jobID to avoid errors and to properly log user activity.

--- application/controllers/Update_materials_used_controller.php
+++ application/controllers/Update_materials_used_controller.php
@@ -16,15 +16,17 @@ class Update_materials_used_controller extends CI_Controller
 
 					if(array_key_exists("materialUnits",$_POST)){
 
-						$this->load->database();
+						if(array_key_exists("jobID",$_POST)){
+							$this->load->database();
 
-						$this->db->query("UPDATE materialsUsed SET materialUnits=".$_POST["materialUnits"]." WHERE materialsUsedID=".$_POST["materialsUsedID"]."");
+							$this->db->query("UPDATE materialsUsed SET materialUnits=".$_POST["materialUnits"]." WHERE materialsUsedID=".$_POST["materialsUsedID"]."");
 
-						$this->db->query("INSERT INTO userLogs(logText,logTimestamp) VALUES ('".$_SESSION["username"]." updated a material for use in jobID #".$_POST["jobID"]."',CURRENT_TIMESTAMP)");
+							$this->db->query("INSERT INTO userLogs(logText,logTimestamp) VALUES ('".$_SESSION["username"]." updated a material for use in jobID #".$_POST["jobID"]."',CURRENT_TIMESTAMP)");
 
-						echo "Updated material";
+							echo "Updated material";
 
-						return;
+							return;
+						}
 					}
 				}
 			}
