66cc98c22cc9ee1317eb3143135c604bff6e03bd
adtcruz <atcruz1@up.edu.ph>
2016-07-04 10:31:13 +0800

    Fixed issues where CURDATE is not recognised

--- db/sample_work_done.sql
+++ db/sample_work_done.sql
@@ -1,10 +1,10 @@
 use cmsc198db;
 
-INSERT INTO work (workDescription, workCost, dateCreated) VALUES ('repair and upgrade', 200, CURDATE ());
-INSERT INTO work (workDescription, workCost, dateCreated) VALUES ('data backup and recovery', 200, CURDATE ());
-INSERT INTO work (workDescription, workCost, dateCreated) VALUES ('software installation', 200, CURDATE ());
-INSERT INTO work (workDescription, workCost, dateCreated) VALUES ('diagnose problem', 0, CURDATE ());
-INSERT INTO work (workDescription, workCost, dateCreated) VALUES ('inspection of equipment', 0, CURDATE ());
-INSERT INTO work (workDescription, workCost, dateCreated) VALUES ('ceiling run', 250, CURDATE ());
-INSERT INTO work (workDescription, workCost, dateCreated) VALUES ('structured cabling and uplink', 300, CURDATE ());
-INSERT INTO work (workDescription, workCost, dateCreated) VALUES ('network device configuration', 300, CURDATE ());
+INSERT INTO work (workDescription, workCost, dateCreated) VALUES ('repair and upgrade', 200, CURDATE());
+INSERT INTO work (workDescription, workCost, dateCreated) VALUES ('data backup and recovery', 200, CURDATE());
+INSERT INTO work (workDescription, workCost, dateCreated) VALUES ('software installation', 200, CURDATE());
+INSERT INTO work (workDescription, workCost, dateCreated) VALUES ('diagnose problem', 0, CURDATE());
+INSERT INTO work (workDescription, workCost, dateCreated) VALUES ('inspection of equipment', 0, CURDATE());
+INSERT INTO work (workDescription, workCost, dateCreated) VALUES ('ceiling run', 250, CURDATE());
+INSERT INTO work (workDescription, workCost, dateCreated) VALUES ('structured cabling and uplink', 300, CURDATE());
+INSERT INTO work (workDescription, workCost, dateCreated) VALUES ('network device configuration', 300, CURDATE());
