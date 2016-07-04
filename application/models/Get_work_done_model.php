a84699e329b3a8ec10b97b0354e57298e8427629
adtcruz <atcruz1@up.edu.ph>
2016-07-04 11:12:19 +0800

    Added model for getting work done in a job

---
+++ application/models/Get_work_done_model.php
@@ -0,0 +1,28 @@
+<?php
+
+class Get_work_done_model extends CI_Model
+{
+	public function __construct ()
+	{
+		parent::__construct ();
+	}
+
+  public function processActions($materialsUsedID){
+		$editOpen = '<a class="btn-floating btn tooltipped waves-effect waves-light cyan" data-position="left" data-delay="50" data-tooltip="Change work duration"';
+		$editClose = '><i class="material-icons">mode_edit</i></a>';
+		$space = '&nbsp;&nbsp;&nbsp;&nbsp;';
+		$deleteOpen = '<a class="btn-floating btn tooltipped waves-effect waves-light red" data-position="left" data-delay="50" data-tooltip="Delete work done"';
+		$deleteClose = '><i class="material-icons">not_interested</i></a>';
+		return $editOpen . $editClose . $space . $deleteOpen . $deleteClose;
+  }
+
+	public function processRate($rate){
+		if ($rate==0) return "No charge";
+		return $rate . " per hour";
+	}
+
+	public function processCost($rate,$workDuration){
+		if($rate==0) return "No charge";
+		return $rate*$workDuration;
+	}
+}
