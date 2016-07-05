b1c01b1742e5386e292242f9ab79f2fcdf499d16
adtcruz <atcruz1@up.edu.ph>
2016-07-05 14:34:12 +0800

    Fixed logging

--- application/controllers/Activate_account_controller.php
+++ application/controllers/Activate_account_controller.php
@@ -20,7 +20,7 @@ class Activate_account_controller extends CI_Controller
 					if($this->db->query("SELECT COUNT(superAdminID) FROM superAdmin WHERE BINARY username='".$_POST["username"]."'")->row_array()["COUNT(superAdminID)"]==1)
 					{
 						$this->db->query("UPDATE superAdmin SET active=1 WHERE BINARY username='".$_POST["username"]."'");
-						$this->db->query("INSERT INTO userLogs(logText, logTimestamp) VALUES ('".$_SESSION["username"]." disabled ".$_POST["username"]."\'s account ',CURRENT_TIMESTAMP)");
+						$this->db->query("INSERT INTO userLogs(logText, logTimestamp) VALUES ('".$_SESSION["username"]." activated ".$_POST["username"]."\'s account ',CURRENT_TIMESTAMP)");
 						echo "Activated account";
 						return;
 					}
@@ -29,7 +29,7 @@ class Activate_account_controller extends CI_Controller
 						if($this->db->query("SELECT COUNT(adminID) FROM adminAcc WHERE BINARY username='".$_POST["username"]."'")->row_array()["COUNT(adminID)"]==1)
 						{
 							$this->db->query("UPDATE adminAcc SET active=1 WHERE BINARY username='".$_POST["username"]."'");
-							$this->db->query("INSERT INTO userLogs(logText, logTimestamp) VALUES ('".$_SESSION["username"]." disabled ".$_POST["username"]."\'s account ',CURRENT_TIMESTAMP)");
+							$this->db->query("INSERT INTO userLogs(logText, logTimestamp) VALUES ('".$_SESSION["username"]." activated ".$_POST["username"]."\'s account ',CURRENT_TIMESTAMP)");
 							echo "Activated account";
 							return;
 						}
--- application/controllers/Deactivate_account_controller.php
+++ application/controllers/Deactivate_account_controller.php
@@ -20,7 +20,7 @@ class Deactivate_account_controller extends CI_Controller
 					if($this->db->query("SELECT COUNT(superAdminID) FROM superAdmin WHERE BINARY username='".$_POST["username"]."'")->row_array()["COUNT(superAdminID)"]==1)
 					{
 						$this->db->query("UPDATE superAdmin SET active=0 WHERE BINARY username='".$_POST["username"]."'");
-						$this->db->query("INSERT INTO userLogs(logText, logTimestamp) VALUES ('".$_SESSION["username"]." disabled ".$_POST["username"]."\'s account ',CURRENT_TIMESTAMP)");
+						$this->db->query("INSERT INTO userLogs(logText, logTimestamp) VALUES ('".$_SESSION["username"]." deactivated ".$_POST["username"]."\'s account ',CURRENT_TIMESTAMP)");
 						echo "Deactivated account";
 						return;
 					}
@@ -29,7 +29,7 @@ class Deactivate_account_controller extends CI_Controller
 						if($this->db->query("SELECT COUNT(adminID) FROM adminAcc WHERE BINARY username='".$_POST["username"]."'")->row_array()["COUNT(adminID)"]==1)
 						{
 							$this->db->query("UPDATE adminAcc SET active=0 WHERE BINARY username='".$_POST["username"]."'");
-							$this->db->query("INSERT INTO userLogs(logText, logTimestamp) VALUES ('".$_SESSION["username"]." disabled ".$_POST["username"]."\'s account ',CURRENT_TIMESTAMP)");
+							$this->db->query("INSERT INTO userLogs(logText, logTimestamp) VALUES ('".$_SESSION["username"]." deactivated ".$_POST["username"]."\'s account ',CURRENT_TIMESTAMP)");
 							echo "Deactivated account";
 							return;
 						}
