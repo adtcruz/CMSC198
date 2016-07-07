fcfc51cc8dfd6cb3a550b9e10707aacf443e8003
clstia <janelliseurolfan@gmail.com>
2016-07-07 16:12:49 +0800

    Implement Select2

---
+++ application/views/Select2_JS_include.php
@@ -0,0 +1,7 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+<script type = "text/javascript" src = "<?= base_url();?>assets/js/jquery-3.0.0.min.js"></script>
+<script type = "text/javascript" src = "<?= base_url();?>assets/js/select2.full.min.js"></script>
+<script type = "text/javascript" src = "<?php echo base_url();?>assets/js/materialize.min.js"></script>
+<script type = "text/javascript">
+    
+</script>
