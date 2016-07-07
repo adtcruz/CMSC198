bed711c4a195a797cfa1aaed728326350ae3ac5f
adtcruz <atcruz1@up.edu.ph>
2016-07-07 16:21:40 +0800

    Added 'Selectable?' column

--- application/models/Manage_selectable_materials_model.php
+++ application/models/Manage_selectable_materials_model.php
@@ -9,16 +9,23 @@ class Manage_selectable_materials_model extends CI_Model
         $this->load->database ();
     }
 
+    public function isSelectable($row)
+    {
+
+      if($row["active"]==1) return "YES";
+      
+      else return "NO";
+    }
+
     public function getTable()
     {
         $query = $this->db->query ('SELECT materialID, materialName, materialDescription, materialCost, materialUnitMeasurement, active FROM materials');
 
-        $this->table->set_heading ('Name', 'Description', 'Cost Per Unit', 'Unit of Measurement');
+        $this->table->set_heading ('Name', 'Description', 'Cost Per Unit', 'Unit of Measurement','Selectable?');
 
         foreach ($query->result_array() as $row)
         {
-
-          $this->table->add_row ($row['materialName'], $row['materialDescription'], $row['materialCost'], $row['materialUnitMeasurement']);
+          $this->table->add_row ($row['materialName'], $row['materialDescription'], $row['materialCost'], $row['materialUnitMeasurement'],$this->isSelectable($row));
         }
         return $this->table->generate();
     }
