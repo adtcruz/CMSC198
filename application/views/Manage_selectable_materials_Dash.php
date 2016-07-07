10f5907ab9583ccabf06337aa08ac27ec36d98d5
adtcruz <atcruz1@up.edu.ph>
2016-07-07 19:10:23 +0800

    Added tabs

--- application/views/Manage_selectable_materials_Dash.php
+++ application/views/Manage_selectable_materials_Dash.php
@@ -5,13 +5,28 @@
 	<div id="mainAppArea" class="col s9 m9 l9 section">
 		<div class="row">
 			<br/>
-			<h3 class="center-align">Manage Selectable Materials</h3>
+			<div class="col s12">
+				<ul class="tabs">
+					<li class="tab col s3"><a class="active" href="#manageMaterials">Manage Selectable Materials</a></li>
+					<li class="tab col s3"><a href="#addMaterial">Add New Material</a></li>
+				</ul>
+			</div>
 			<br/>
-				<?php
-					echo $materialsTable;
-				?>
-			<br/>
-			<br/>
+			<div id="manageMaterials">
+				<br/>
+				<h3 class="center-align">Manage Selectable Materials</h3>
+				<br/>
+					<?php
+						echo $materialsTable;
+					?>
+				<br/>
+				<br/>
+			</div>
+			<div id="addMaterial">
+				<br/>
+				<h3 class="center-align">Add New Material</h3>
+				<br/>
+			</div>
 		</div>
 	</div>
 </div>
