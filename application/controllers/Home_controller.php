eddda38f3ffa2c81cef0e40ed719087d9f52c322
clstia <janelliseurolfan@gmail.com>
2016-07-08 14:43:03 +0800

    Prepared switch-case for proper view loading

--- application/controllers/Home_controller.php
+++ application/controllers/Home_controller.php
@@ -3,18 +3,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home_controller extends CI_Controller
 {
-	public function index ()
+    public function __construct ()
+    {
+        parent::__construct ();
+        session_start();
+        $this->load->model ('Home_dash_model', 'hdm');
+    }
+
+    public function index ()
 	{
-
-		session_start();
-
-		if(array_key_exists("type",$_SESSION)){
-
-			$this->load->view('Home_view');
+		if(array_key_exists("type",$_SESSION))
+        {
+            /*
+            switch ($_SESSION['type'])
+            {
+                case 'client':
+                    $db_data = $this->hdm->getClientData ($_SESSION['username']);
+                break;
+                case 'admin':
+                    $db_data = $this->hdm->getAdminData ($_SESSION['username']);
+                break;
+                case 'superadmin':
+                    $db_data = $this->hdm->getSAData ($_SESSION['username']);
+                break;
+                default:
+                    $this->load->view ('Login_view');
+            }*/
+            $this->load->view ('Home_view');
 		}
-
-		else {
-
+		else
+        {
 			$this->load->view('Login_view');
 		}
 	}
