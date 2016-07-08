8d5e334ad0a9c17d6125cbdd9b0104388eba1e4d
Azha Vianca de Belen <azhaviancadebelen@gmail.com>
2016-07-08 14:40:43 +0800

    home UI for each user types

---
+++ application/views/Home_Dash_Client.php
@@ -0,0 +1,199 @@
+<div class="row">
+	<?php $this->load->view('Sidenav');?>
+	<!-- FILLER TO PUSH MAIN CONTENT TO THE RIGHT -->
+	<div class="col s3 m3 l3"><br/><br/></div>
+	<!-- MAIN CONTENT CONTAINER -->
+	<div class="col s9 m9 l9">
+		<div class="row">
+			<div class="col s1 m1 l1">&nbsp;</div>
+			<div class="col s10 m10 l10">
+				<br/><br/>
+				   <div class="col s12 m12">
+			          <div class="card blue-grey darken-1">
+			            <div class="card-content white-text">
+			              <br/>
+							<h5 class="center-align">Recent Job Requests</h5>
+						  <br/>
+			              <table class="bordered">
+					        <thead>
+					          <tr>
+					              <th data-field="id">Job Description</th>
+					              <th data-field="name">Date Submitted</th>
+					              <th data-field="price">Status</th>
+					          </tr>
+					        </thead>
+
+					        <tbody>
+					          <tr>
+					            <td></td>
+					            <td></td>
+					            <td></td>
+					          </tr>
+					          <tr>
+					            <td></td>
+					            <td></td>
+					            <td></td>
+					          </tr>
+					          <tr>
+					            <td></td>
+					            <td></td>
+					            <td></td>
+					          </tr>
+					          <tr>
+					            <td></td>
+					            <td></td>
+					            <td></td>
+					          </tr>
+					          <tr>
+					            <td></td>
+					            <td></td>
+					            <td></td>
+					          </tr>
+					          <tr>
+					            <td></td>
+					            <td></td>
+					            <td></td>
+					          </tr>
+					        </tbody>
+					      </table>
+			            </div>
+			          </div>
+			        </div>
+			    
+			    <br/><br/>
+			    	<div class="col s12 m4 l4">
+			          <div class="card blue-grey darken-1">
+			            <div class="card-content white-text">
+			              <br/>
+							<h5 class="center-align">Common Problems Encountered</h5>
+						  <br/>
+						  <table class="bordered centered">
+					        <thead>
+					          <tr>
+					              <th data-field="id">Problems</th>
+					          </tr>
+					        </thead>
+
+					        <tbody>
+					          <tr>
+					            <td></td>
+					          </tr>
+					          <tr>
+					            <td></td>
+					          </tr>
+					          <tr>
+					            <td></td>
+					          </tr>
+					          <tr>
+					            <td></td>
+					          </tr>
+					          <tr>
+					            <td></td>
+					          </tr>
+					          <tr>
+					            <td></td>
+					          </tr>
+					        </tbody>
+					      </table>
+			            </div>
+			          </div>
+			        </div>
+				   <div class="col s12 m4 l4">
+			          <div class="card blue-grey darken-1">
+			            <div class="card-content white-text">
+			              <br/>
+							<h5 class="center-align">Common Services Required</h5>
+						  <br/>
+						  <table class="bordered centered">
+					        <thead>
+					          <tr>
+					              <th data-field="id">Services</th> 
+					          </tr>
+					        </thead>
+
+					        <tbody>
+					          <tr>
+					            <td></td>
+					          </tr>
+					          <tr>
+					            <td></td>
+					          </tr>
+					          <tr>
+					            <td></td>
+					          </tr>
+					          <tr>
+					            <td></td>
+					          </tr>
+					          <tr>
+					            <td></td>
+					          </tr>
+					          <tr>
+					            <td></td>
+					          </tr>
+					        </tbody>
+					      </table>
+			            </div>
+			          </div>
+			        </div>
+			        <div class="col s12 m64 l4">
+			          <div class="card blue-grey darken-1">
+			            <div class="card-content white-text">
+			              <br/>
+							<h5 class="center-align">Work Rates</h5>
+						  <br/>
+			              <table class="bordered centered">
+					        <thead>
+					          <tr>
+					              <th data-field="id">Work</th>
+					              <th data-field="name">Rate</th> 
+					          </tr>
+					        </thead>
+
+					        <tbody>
+					          <tr>
+					            <td></td>
+					            <td></td>
+					          </tr>
+					          <tr>
+					            <td></td>
+					            <td></td>
+					          </tr>
+					          <tr>
+					            <td></td>
+					            <td></td>
+					          </tr>
+					          <tr>
+					            <td></td>
+					            <td></td>
+					          </tr>
+					          <tr>
+					            <td></td>
+					            <td></td>
+					          </tr>
+					          <tr>
+					            <td></td>
+					            <td></td>
+					          </tr>
+					        </tbody>
+					      </table>
+							
+						  	</p>
+						  </div>
+			            </div>
+			          </div>
+			        </div>
+			    
+			    <br/><br/>
+			</div>
+			<div class="col s1 m1 l1">&nbsp;</div>
+			<div class="row"></div>
+
+	     
+		</div>
+
+	</div>
+
+
+
+
+</div>
