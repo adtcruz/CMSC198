bcfbed3f4cf5179eeb04f9aa7364ab48250695d5
clstia <janelliseurolfan@gmail.com>
2016-07-08 08:05:35 +0800

    Fixed Select2JS

--- application/views/Job_requests_office_selector.php
+++ application/views/Job_requests_office_selector.php
@@ -1,5 +1,5 @@
 <div class="input-field">
-	<i class="material-icons large prefix">my_location</i>
+	<i class="material-icons">my_location</i>
 	<select id="office-selector" class="dropSelect" name="office-selector" onchange="getUsers('<?php echo base_url();?>');">
         <option disabled selected> </option>
         <?php echo $options;?>
