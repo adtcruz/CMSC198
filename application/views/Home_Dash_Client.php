b4139b5ac0492e7258c3d7502aa313997451424a
clstia <janelliseurolfan@gmail.com>
2016-07-08 15:31:47 +0800

    Partial client view

--- application/views/Home_Dash_Client.php
+++ application/views/Home_Dash_Client.php
@@ -1,199 +1,84 @@
 <div class="row">
-	<?php $this->load->view('Sidenav');?>
-	<!-- FILLER TO PUSH MAIN CONTENT TO THE RIGHT -->
-	<div class="col s3 m3 l3"><br/><br/></div>
-	<!-- MAIN CONTENT CONTAINER -->
-	<div class="col s9 m9 l9">
-		<div class="row">
-			<div class="col s1 m1 l1">&nbsp;</div>
-			<div class="col s10 m10 l10">
-				<br/><br/>
-				   <div class="col s12 m12">
-			          <div class="card blue-grey darken-1">
-			            <div class="card-content white-text">
-			              <br/>
-							<h5 class="center-align">Recent Job Requests</h5>
-						  <br/>
-			              <table class="bordered">
-					        <thead>
-					          <tr>
-					              <th data-field="id">Job Description</th>
-					              <th data-field="name">Date Submitted</th>
-					              <th data-field="price">Status</th>
-					          </tr>
-					        </thead>
+    <?php $this->load->view('Sidenav');?>
+    <!-- FILLER TO PUSH MAIN CONTENT TO THE RIGHT -->
+    <div class="col s3 m3 l3"><br/><br/></div>
+    <!-- MAIN CONTENT CONTAINER -->
+    <div class="col s9 m9 l9">
+        <div class="row">
+            <div class="col s1 m1 l1">&nbsp;</div>
+            <div class="col s10 m10 l10">
+                <br/><br/>
+                <div class="col s12 m12">
+                    <!-- Lastest Jobs start -->
+                    <div class="card blue-grey darken-1">
+                        <div class="card-content white-text">
+                            <br/>
+                            <h5 class="center-align">Recent Job Requests</h5>
+                            <?php
+                                $template = array ('table_open' => '<table class = "bordered">');
+                                $this->table->set_template ($template);
+                                $this->table->set_heading ('Job Description', 'Date Submitted', 'Status');
+                                foreach ($latestJobs as $row)
+                                {
+                                $this->table->add_row ($row['description'], $row['dateCreated'], $row['status']);
+                                }
+                                echo $this->table->generate ();
+                                $this->table->clear ();
+                            ?>
+                        </div>
+                    </div>
+                    <!-- Lastest Jobs end -->
 
-					        <tbody>
-					          <tr>
-					            <td></td>
-					            <td></td>
-					            <td></td>
-					          </tr>
-					          <tr>
-					            <td></td>
-					            <td></td>
-					            <td></td>
-					          </tr>
-					          <tr>
-					            <td></td>
-					            <td></td>
-					            <td></td>
-					          </tr>
-					          <tr>
-					            <td></td>
-					            <td></td>
-					            <td></td>
-					          </tr>
-					          <tr>
-					            <td></td>
-					            <td></td>
-					            <td></td>
-					          </tr>
-					          <tr>
-					            <td></td>
-					            <td></td>
-					            <td></td>
-					          </tr>
-					        </tbody>
-					      </table>
+                    <!-- Top 5 Common Problems -->
+                    <div class="col s12 m4 l4">
+                        <div class="card blue-grey darken-1">
+                            <div class="card-content white-text">
+                                <br/>
+                                <h5 class="center-align">Common Problems Encountered</h5>
+                                <?php
+                                    $template = array ('table_open' => '<table class = "bordered centered">');
+                                    $this->table->set_template ($template);
+                                    foreach ($mostCommonProblems as $row)
+                                    {
+                                        $this->table->add_row ($row['description'].' ('.$row['count'].')');
+                                    }
+                                    echo $this->table->generate ();
+                                    $this->table->clear ();
+                                ?>
+                            </div>
 			            </div>
-			          </div>
-			        </div>
-			    
-			    <br/><br/>
-			    	<div class="col s12 m4 l4">
-			          <div class="card blue-grey darken-1">
-			            <div class="card-content white-text">
-			              <br/>
-							<h5 class="center-align">Common Problems Encountered</h5>
-						  <br/>
-						  <table class="bordered centered">
-					        <thead>
-					          <tr>
-					              <th data-field="id">Problems</th>
-					          </tr>
-					        </thead>
+                    </div>
 
-					        <tbody>
-					          <tr>
-					            <td></td>
-					          </tr>
-					          <tr>
-					            <td></td>
-					          </tr>
-					          <tr>
-					            <td></td>
-					          </tr>
-					          <tr>
-					            <td></td>
-					          </tr>
-					          <tr>
-					            <td></td>
-					          </tr>
-					          <tr>
-					            <td></td>
-					          </tr>
-					        </tbody>
-					      </table>
-			            </div>
-			          </div>
-			        </div>
-				   <div class="col s12 m4 l4">
-			          <div class="card blue-grey darken-1">
-			            <div class="card-content white-text">
-			              <br/>
-							<h5 class="center-align">Common Services Required</h5>
-						  <br/>
-						  <table class="bordered centered">
-					        <thead>
-					          <tr>
-					              <th data-field="id">Services</th> 
-					          </tr>
-					        </thead>
-
-					        <tbody>
-					          <tr>
-					            <td></td>
-					          </tr>
-					          <tr>
-					            <td></td>
-					          </tr>
-					          <tr>
-					            <td></td>
-					          </tr>
-					          <tr>
-					            <td></td>
-					          </tr>
-					          <tr>
-					            <td></td>
-					          </tr>
-					          <tr>
-					            <td></td>
-					          </tr>
-					        </tbody>
-					      </table>
-			            </div>
-			          </div>
-			        </div>
-			        <div class="col s12 m64 l4">
-			          <div class="card blue-grey darken-1">
-			            <div class="card-content white-text">
-			              <br/>
-							<h5 class="center-align">Work Rates</h5>
-						  <br/>
-			              <table class="bordered centered">
-					        <thead>
-					          <tr>
-					              <th data-field="id">Work</th>
-					              <th data-field="name">Rate</th> 
-					          </tr>
-					        </thead>
-
-					        <tbody>
-					          <tr>
-					            <td></td>
-					            <td></td>
-					          </tr>
-					          <tr>
-					            <td></td>
-					            <td></td>
-					          </tr>
-					          <tr>
-					            <td></td>
-					            <td></td>
-					          </tr>
-					          <tr>
-					            <td></td>
-					            <td></td>
-					          </tr>
-					          <tr>
-					            <td></td>
-					            <td></td>
-					          </tr>
-					          <tr>
-					            <td></td>
-					            <td></td>
-					          </tr>
-					        </tbody>
-					      </table>
-							
-						  	</p>
-						  </div>
-			            </div>
-			          </div>
-			        </div>
-			    
-			    <br/><br/>
-			</div>
-			<div class="col s1 m1 l1">&nbsp;</div>
-			<div class="row"></div>
-
-	     
-		</div>
-
-	</div>
-
-
-
-
+                    <!-- Some Work Rates -->
+                    <div class="col s12 m64 l4">
+                        <div class="card blue-grey darken-1">
+                            <div class="card-content white-text">
+                                <br/>
+                                <h5 class="center-align">Work Rates</h5>
+                                <?php
+                                    $template = array ('table_open' => '<table class = "bordered centered">');
+                                    $this->table->set_template ($template);
+                                    $this->table->set_heading ('Work', 'Rate');
+                                    foreach ($services as $row)
+                                    {
+                                        $serviceRate = $row['serviceRate'];
+                                        if ($serviceRate === '0')
+                                        {
+                                            $serviceRate = 'Free';
+                                        }
+                                        $this->table->add_row ($row['serviceName'], $serviceRate);
+                                    }
+                                    echo $this->table->generate ();
+                                    $this->table->clear ();
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Some Work Rates -->
+                </div>
+            </div>
+            <div class="col s1 m1 l1">&nbsp;</div>
+            <div class="row"></div>
+        </div>
+    </div>
 </div>
