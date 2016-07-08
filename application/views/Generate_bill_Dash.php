1bd9a9c6ca35215734153d980a72344734770ff6
clstia <janelliseurolfan@gmail.com>
2016-07-08 09:08:41 +0800

    Added "Job Request Form" button

--- application/views/Generate_bill_Dash.php
+++ application/views/Generate_bill_Dash.php
@@ -1,43 +1,40 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-<div class="row">
-	<?php $this->load->view('Sidenav');?>
-	<div id="navarea" class="col s3 m3 l3 section"><br/><br/></div>
-	<div id="mainc" class="col s9 m9 l9 section">
-		<div class="row">
-			<form autocomplete="off">
-				<br/>
-				<h3 class="center-align">Generate Bill</h3>
-				<br/>
-					<?php
-					$this->load->library ('table');
-					$template = array (
-							'table_open' => '<table class="bordered centered highlight">'
-						);
-					$this->table->set_template ($template);
-					$this->table->set_heading ('Job ID', 'Client Name', '');
-					if(count($generateBill_array) == 0){
-						echo "<h5 class=\"center-align\">Sorry, there are no finished jobs at the moment.</h5>";
-						return;
-					}
-					foreach ($generateBill_array as $row){
-						$this->table->add_row (
-              $row['jobID'],
-              $row['givenName'].' '.$row['lastName'],
-              array(
-  							'data' => '<a href="topdf_bfp/'.$row['jobID'].'" target = "_blank" class="waves-effect waves-light btn blue-grey">Generate Bill</a>'
-  						)
-						);
-					}
-					echo $this->table->generate ();
-				?>
-				<br/>
-				<br/>
-			</form>
-		</div>
-	</div>
-</div>
-
-
-
+    <div class="row">
+        <?php $this->load->view('Sidenav');?>
+        <div id="navarea" class="col s3 m3 l3 section"><br/><br/></div>
+        <div id="mainc" class="col s9 m9 l9 section">
+            <div class="row">
+                <form autocomplete="off">
+                <br/>
+    			<h3 class="center-align">Generate Bill</h3>
+    			<br/>
+    			<?php
+                    $this->load->library ('table');
+    				$template = array(
+                        'table_open' => '<table class="bordered centered highlight">'
+    				);
+    				$this->table->set_template ($template);
+    				$this->table->set_heading ('Job ID', 'Client Name', '');
+    				if(count($generateBill_array) == 0)
+                    {
+                        echo "<h5 class=\"center-align\">Sorry, there are no finished jobs at the moment.</h5>";
+    					return;
+    				}
+    				foreach ($generateBill_array as $row)
+                    {
+                        $this->table->add_row (
+                            $row['jobID'],
+                            $row['givenName'].' '.$row['lastName'],
+                            '<a href = "topdf_bfp/'.$row['jobID'].'" target = "_blank" class= "waves-effect waves-light btn blue-grey">Generate Bill</a>&nbsp;&nbsp;&nbsp;<a href = "topdf_jrf/'.$row['jobID'].'" target = "_blank" class= "waves-effect waves-light btn blue-grey">Job Request Form</a>'
+                        );
+    				}
+    				echo $this->table->generate ();
+    			?>
+    			<br/>
+    			<br/>
+    			</form>
+    		</div>
+	    </div>
+    </div>
 </body>
 </html>
