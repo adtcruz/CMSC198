4657908011c94dc9aa049482016d0e8a25912a16
clstia <janelliseurolfan@gmail.com>
2016-07-08 16:35:51 +0800

    View for Admin users

---
+++ application/views/Home_Dash_Admin.php
@@ -0,0 +1,47 @@
+<div class="row">
+    <?php $this->load->view('Sidenav');?>
+    <!-- FILLER TO PUSH MAIN CONTENT TO THE RIGHT -->
+    <div class="col s3 m3 l3"><br/><br/></div>
+        <!-- MAIN CONTENT CONTAINER -->
+    <div class="col s9 m9 l9">
+        <div class="row">
+            <div class="col s1 m1 l1">&nbsp;</div>
+            <div class="col s10 m10 l10">
+                <br/><br/>
+
+                <div class="col s12 m12">
+                    <div class="card blue-grey darken-1">
+                        <div class="card-content white-text">
+                            <br/>
+                            <h3 class="center-align">Welcome to ITCBS Billing System!</h3>
+                            <br/>
+                        </div>
+                    </div>
+                </div>
+                <br/><br/>
+
+                <!-- card 1 -->
+                <div class="col s12 m6 l6">
+                    <div class="card blue-grey darken-1">
+                        <div class="card-content white-text">
+                            <h5 class="center-align">Pending Job Request</h5>
+                        </div>
+                    </div>
+                </div>
+                <!-- card 1 -->
+
+                <div class="col s12 m6 l6">
+                    <div class="card blue-grey darken-1">
+                        <div class="card-content white-text">
+                            <h5 class="center-align">Pending Job Request</h5>
+                        </div>
+                    </div>
+                </div>
+
+            <br/><br/>
+            </div>
+            <div class="col s1 m1 l1">&nbsp;</div>
+            <div class="row"></div>
+        </div>
+    </div>
+</div>
