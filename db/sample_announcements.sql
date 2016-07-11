88905ccd8353e44f79c586f41427c89e80d21557
clstia <janelliseurolfan@gmail.com>
2016-07-11 14:24:07 +0800

    Added sample entries for "Announcements"

---
+++ db/sample_announcements.sql
@@ -0,0 +1,16 @@
+use cmsc198db;
+
+INSERT INTO annoucements (announcementText, announcementTitle, createdBy, createdByType, dateCreated)
+VALUES ('This is the announcement for Announcement 1', 'Announcement 1', 1, 'superadmin', CURDATE());
+
+INSERT INTO annoucements (announcementText, announcementTitle, createdBy, createdByType, dateCreated)
+VALUES ('This is the announcement for Announcement 2', 'Announcement 2', 1, 'superadmin', CURDATE());
+
+INSERT INTO annoucements (announcementText, announcementTitle, createdBy, createdByType, dateCreated)
+VALUES ('This is the announcement for Announcement 3', 'Announcement 3', 1, 'superadmin', CURDATE());
+
+INSERT INTO annoucements (announcementText, announcementTitle, createdBy, createdByType, dateCreated)
+VALUES ('This is the announcement for Announcement 4', 'Announcement 4', 1, 'superadmin', CURDATE());
+
+INSERT INTO annoucements (announcementText, announcementTitle, createdBy, createdByType, dateCreated)
+VALUES ('This is the announcement for Announcement 5', 'Announcement 5', 1, 'superadmin', CURDATE());
