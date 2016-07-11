665ade16cf6186ebef3c0d72444bcefba849f731
clstia <janelliseurolfan@gmail.com>
2016-07-11 10:57:39 +0800

    Added files for "Announcements"

---
+++ application/controllers/Announcements_controller.php
@@ -0,0 +1,32 @@
+<?php
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Announcements_controller extends CI_Controller
+{
+    public function __construct ()
+    {
+        parent::__construct ();
+        $this->load->model ('Announcements_model', 'am');
+    }
+
+    public function index ()
+    {
+        //$db_data = $this->am->getAnnouncements ();
+    }
+
+    public function add ()
+    {
+        session_start ();
+        if (array_key_exists ("type", $_SESSION))
+        {
+            if ($_SESSION['type'] != 'client')
+            {
+                $db_data['title'] = $this->input->post ('annTitle');
+                $db_data['text'] = $this->input->post ('annText');
+                $db_data['createdBy'] = $_SESSION['username'];
+                $db_data['createdByType'] = $_SESSION['type'];
+                $this->am->addAnnouncement ($db_data);
+            }
+        }
+    }
+}
+?>
---
+++ application/models/Announcements_model.php
@@ -0,0 +1,25 @@
+<?php
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Announcements_model extends CI_Model
+{
+    public function __construct ()
+    {
+        parent::__construct ();
+        $this->load->database ();
+    }
+
+    public function getAnnouncements ()
+    {
+        $query = $this->db->query ('SELECT announcements.announcementText AS details, announcements.announcementTitle AS title, announcements.dateCreated FROM annoucements ORDER BY announcements.dateCreated DESC LIMIT 3');
+        $db_data['announcements'] = $query->result_array();
+        return $db_data;
+    }
+
+    public function addAnnouncement ($db_data)
+    {
+        // extract user ID using user type and username
+        echo $db_data['createdBy'];
+    }
+}
+?>
