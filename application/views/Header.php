da16d84fedebfc3e42029c34543a86b6c0b149f3
adtcruz <atcruz1@up.edu.ph>
2016-07-11 11:23:09 +0800

    Changed from 'ITCBS' to 'ITC Billing System'

--- application/views/Header.php
+++ application/views/Header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>ITCBS</title>
+		<title>ITC Billing System</title>
 		<meta charset="utf-8" lang="en-gb"/>
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/materialize.min.css">
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/material-design-icons.css">
