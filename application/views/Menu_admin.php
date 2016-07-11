fc396553817cd16733fcf6592f0e390a2b3fdb2b
clstia <janelliseurolfan@gmail.com>
2016-07-11 18:24:10 +0800

    Refined dropdown action for "Manage Application" button

--- application/views/Menu_admin.php
+++ application/views/Menu_admin.php
@@ -1,3 +1,5 @@
+<?php
+/*
 <li id="viewJRButton"><a id="viewJRItem" class="waves-effect waves-light white-text" href="<?php base_url();?>job_requests">Job Requests</a></li>
 <li id="viewSchedButton"><a id="viewSchedItem" class="waves-effect waves-light white-text" href="<?php base_url();?>view_schedule">View Schedule</a></li>
 <li id="genBillButton"><a id="gnrtBlItem" class="waves-effect waves-light white-text" href="<?php base_url();?>generate_bill">Generate Bill</a></li>
@@ -16,3 +18,21 @@
     <li><a href="<?php base_url();?>manage_selectable_work">Manage Work</a></li>
     <li><a href="<?php base_url();?>manage_selectable_materials">Manage Materials</a></li>
   </ul>
+*/
+?>
+
+<li id="viewJRButton"><a id="viewJRItem" class="waves-effect waves-light white-text" href="<?php base_url();?>job_requests">Job Requests</a></li>
+<li id="viewSchedButton"><a id="viewSchedItem" class="waves-effect waves-light white-text" href="<?php base_url();?>view_schedule">View Schedule</a></li>
+<li id="genBillButton"><a id="gnrtBlItem" class="waves-effect waves-light white-text" href="<?php base_url();?>generate_bill">Generate Bill</a></li>
+<li id = "mngeApButton">
+    <ul class="collapsible" data-collapsible="accordion">
+        <li>
+            <div class="collapsible-header">Manage Application</div>
+            <div class="collapsible-body">
+                    <a class = "waves-effect waves-light grey darken-4 white-text" href = "<?php base_url();?>manage_accounts">Manage Accounts</a>
+                    <a class = "waves-effect waves-light grey darken-4 white-text" href = "<?php base_url();?>manage_selectable_work">Manage Work</a>
+                    <a class = "waves-effect waves-light grey darken-4 white-text" href = "<?php base_url();?>manage_selectable_materials">Manage Materials</a>
+            </div>
+        </li>
+    </ul>
+</li>
