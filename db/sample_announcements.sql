267261ce3b5604ea31d341a6d563063a96139acf
clstia <janelliseurolfan@gmail.com>
2016-07-11 17:18:12 +0800

    Fixed typo error

--- db/sample_announcements.sql
+++ db/sample_announcements.sql
@@ -1,7 +1,9 @@
 use cmsc198db;
 
 INSERT INTO announcements (announcementText, announcementTitle, createdBy, createdByType, dateCreated)
-VALUES ('This is the announcement for Announcement 1', 'Announcement 1', 1, 'superadmin', CURDATE());
+VALUES ('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla enim ligula, condimentum dapibus lacus ac, fringilla elementum eros. Sed a accumsan sem. Nulla ornare mauris vel elit viverra sodales. Nam cursus lectus ultricies odio dictum, et mollis eros sodales. Donec consequat, nisl sit amet venenatis luctus, elit augue congue risus, vel placerat justo libero eu velit. Morbi et lorem neque. Sed vel urna luctus, consectetur felis at, fringilla massa. Aliquam porta nec sapien ac egestas. Curabitur et tincidunt ante. Curabitur aliquam, magna ullamcorper lobortis gravida, neque mi rutrum sapien, vel ultrices eros mi eget tortor.
+
+Vestibulum consequat viverra nibh ac lacinia. Pellentesque eu urna nec lectus malesuada pretium vel eu tortor. Suspendisse vel consectetur eros. Ut odio tellus, posuere in semper sed, tempor vitae nulla. Donec ultrices vehicula ornare. In tortor arcu, placerat at eros ut, posuere malesuada metus. Vestibulum dictum vestibulum aliquet. Sed tincidunt justo at finibus accumsan. Donec posuere.', 'Announcement 1', 1, 'superadmin', CURDATE());
 
 INSERT INTO announcements (announcementText, announcementTitle, createdBy, createdByType, dateCreated)
 VALUES ('This is the announcement for Announcement 2', 'Announcement 2', 1, 'superadmin', CURDATE());
