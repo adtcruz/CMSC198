e50ae9882960d2eca105b1b0fad88829c1caec8e
clstia <janelliseurolfan@gmail.com>
2016-07-11 15:14:52 +0800

    Removed "Home_Dash". Integrated it to "Home_controller"

--- application/views/Home_Dash.php
+++
@@ -1,9 +0,0 @@
-<?php
-	if($_SESSION["type"]==="client") $this->load->view('Home_Dash_Client');
-		
-	if($_SESSION["type"]==="technician") $this->load->view('Home_Dash_Technician');
-
-	if($_SESSION["type"]==="admin") $this->load->view('Home_Dash_Admin');
-
-	if($_SESSION["type"]==="superadmin") $this->load->view('Home_SuperAdmin');	
-?>
