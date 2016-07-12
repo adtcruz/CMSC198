a8f8ae063590fc3051b97a6648a0836008546e34
clstia <janelliseurolfan@gmail.com>
2016-07-12 16:03:50 +0800

    Added contents for UI Fixing

--- application/views/Announcements_view.php
+++ application/views/Announcements_view.php
@@ -12,8 +12,33 @@
             <li class="tab col s12"><a href="#annViewTab"> annViewTab </a></li>
         </ul>
     </div>
-    <div id="annAddTab" class="col s12"> annAddTab </div>
-    <div id="annViewTab" class="col s12"> annViewTab </div>
+    <div id="annAddTab" class="col s12">
+        <!-- Announcements Add -->
+        <?php
+            $this->load->helper ('form');
+            echo form_open ();
+            echo form_label ('Title', 'title');
+            echo form_input ('title', 'title');
+            echo form_label ('Text', 'text');
+            echo form_textarea ('text', 'text');
+            echo form_submit ();
+            echo form_reset ();
+            echo form_close ();
+        ?>
+    </div>
+    <div id="annViewTab" class="col s12">
+        <!-- Announcements View -->
+        <?php
+            $lines = array ();
+            foreach ($announcements as $row)
+            {
+                $line = '<div class = "collapsible-header blue-grey darken-1 white-text">'.$row['title'].'</div><div class = "collapsible-body">'.$row['details'].'</div>';
+                $lines[] = $line;
+            }
+            $attributes = array ('class' => 'collapsible', 'data-collapsible' => 'accordion');
+            echo ul ($lines, $attributes);
+        ?>
+    </div>
 </div>
 
 
