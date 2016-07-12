d60d848fcc94291227c9717f0dfc4a33406f9852
clstia <janelliseurolfan@gmail.com>
2016-07-12 17:05:16 +0800

    Moved JS to another file. Added character counter for textarea

--- application/views/Announcements_view.php
+++ application/views/Announcements_view.php
@@ -1,79 +1,82 @@
-<?php
-    if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-    $this->load->view ('Header');
-    $this->load->view ('Sidenav');
-?>
-
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
 <div class="row">
-    <div class="col s3 m3 l3"><br/></div>
+    <?php
+        $this->load->view('Header');
+        $this->load->view('Sidenav');
+    ?>
+    <div id="navarea" class="col s3 m3 l3 section"><br/><br/></div>
     <div class="col s9 m9 l9">
         <ul class="tabs">
             <li class="tab col s12"><a href="#annAddTab"> annAddTab </a></li>
             <li class="tab col s12"><a href="#annViewTab"> annViewTab </a></li>
         </ul>
     </div>
-    <div id="annAddTab" class="col s12">
-        <!-- Announcements Add -->
-        <?php
-            $this->load->helper ('form');
-            echo form_open ('', 'id="addAnnForm"');
-            echo form_hidden ('userDetails', array (
-                'username' => $_SESSION['username'],
-                'type' => $_SESSION['type']
-            ));
-            echo form_label ('Title', 'title');
-            $param = array (
-                'name' => 'title',
-                'id' => 'title',
-                'placeholder' => 'Enter Announcement Title',
-                'maxlength' => '128',
-                'size' => '20'
-            );
-            echo form_input ($param);
-            echo form_label ('Text', 'text');
-            $param = array (
-                'name' => 'content',
-                'id' => 'content',
-                'placeholder' => 'Enter Announcement Content',
-                'maxlength' => '1024',
-                'rows' => '20',
-                'cols' => '100',
-                'style' => 'resize: none'
-            );
-            echo form_textarea ($param);
-            echo form_submit ('Add Announcement');
-            echo form_reset ('Reset Form');
-            echo form_close ();
-        ?>
+    <div id="mainAppArea" class="col s9 m9 l9 section">
+        <div class="row">
+            <br/>
+            <br/>
+            <div id="annAddTab" class="col s12 m12 l12">
+                <!-- Announcements Add -->
+                <?php
+                    $this->load->helper ('form');
+                    echo form_open ('', 'id="addAnnForm"');
+                    echo form_hidden ('userDetails', array (
+                        'username' => $_SESSION['username'],
+                        'type' => $_SESSION['type']
+                    ));
+                    echo form_label ('Title', 'title');
+                    $param = array (
+                        'name' => 'title',
+                        'id' => 'title',
+                        'placeholder' => 'Enter Announcement Title',
+                        'maxlength' => '128',
+                        'size' => '20'
+                    );
+                    echo form_input ($param);
+                    echo form_label ('Text', 'text');
+                    $param = array (
+                        'name' => 'content',
+                        'id' => 'content',
+                        'placeholder' => 'Enter Announcement Content',
+                        'maxlength' => '1024',
+                        'style' => 'resize: none; height: 200px',
+                        'onkeyup' => 'countChar(this)'
+                    );
+                    echo form_textarea ($param);
+                    echo '<div id="charCount"></div>';
+                    echo form_submit ('addAnn','Add Announcement');
+                    echo form_reset ('reset','Reset Form');
+                    echo form_close ();
+                ?>
+            </div>
+            <br/>
+            <br/>
+        </div>
     </div>
-    <div id="annViewTab" class="col s12">
-        <!-- Announcements View -->
-        <?php
-            $lines = array ();
-            foreach ($announcements as $row)
-            {
-                $line = '<div class = "collapsible-header blue-grey darken-1 white-text">'.$row['title'].'</div><div class = "collapsible-body">'.$row['details'].'</div>';
-                $lines[] = $line;
-            }
-            $attributes = array ('class' => 'collapsible', 'data-collapsible' => 'accordion');
-            echo ul ($lines, $attributes);
-        ?>
+    <div id="mainAppArea" class="col s9 m9 l9 section">
+        <div class="row">
+            <div id="annViewTab" class="col s12 m12 l12">
+                <!-- Announcements View -->
+                <?php
+                    $lines = array ();
+                    foreach ($announcements as $row)
+                    {
+                        $line = '<div class = "collapsible-header blue-grey darken-1 white-text">'.$row['title'].'</div><div class = "collapsible-body">'.$row['details'].'</div>';
+                        $lines[] = $line;
+                    }
+                    $attributes = array ('class' => 'collapsible', 'data-collapsible' => 'accordion');
+                    echo ul ($lines, $attributes);
+                ?>
+            </div>
+            <br/>
+        </div>
     </div>
 </div>
-
-
 <?php
     $this->load->view ('Common_scripts');
     $this->load->view ('Logout_script');
+    $this->load->view ('Announcements_script');
 ?>
-<script type="text/javascript">
-    $('document').ready(
-        function ()
-        {
-            $('#annButton').addClass("black");
-            $('ul.tabs').tabs();
-        }
-    );
-</script>
 </body>
+
 </html>
