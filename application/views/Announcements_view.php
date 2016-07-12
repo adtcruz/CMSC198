aa055ddeb48dfa8b0a710babc86522212db045af
clstia <janelliseurolfan@gmail.com>
2016-07-12 15:25:22 +0800

    Adding tabs for "Add Announcement" and "View Announcements"

--- application/views/Announcements_view.php
+++ application/views/Announcements_view.php
@@ -11,7 +11,10 @@ $this->load->view ('Header');
     <!-- FILLER TO PUSH MAIN CONTENT TO THE RIGHT -->
     <div class="col s3 m3 l3"><br/><br/></div>
     <!-- MAIN CONTENT CONTAINER -->
-    <div class="col s9 m9 l9">
+
+    <div id="annAddTab">
+    </div>
+    <div id="annMainTab" class="col s9 m9 l9">
         <br/><br/>
         <div class="row">
             <div class="col s1 m1 l1">&nbsp;</div>
