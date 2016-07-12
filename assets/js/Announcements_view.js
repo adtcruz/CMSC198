3a1692e3f70805132ae68a8927e214ddde6a6143
clstia <janelliseurolfan@gmail.com>
2016-07-12 17:05:57 +0800

    Essential on-load script for "Announcements"

---
+++ assets/js/Announcements_view.js
@@ -0,0 +1,24 @@
+$('document').ready(
+    function ()
+    {
+        $('#annButton').addClass("black");
+        $('ul.tabs').tabs();
+        $('#charCount').text('1024 characters left');
+        $('#content').keyup(
+            function ()
+            {
+                var max = 1024;
+                var len = $(this).val().length;
+                if (len >= max)
+                {
+                    $('#charCount').text(' you have reached the limit');
+                }
+                else
+                {
+                    var char = max - len;
+                    $('#charCount').text(char + ' characters left');
+                }
+            }
+        );
+    }
+);
