2ce76dc48ff39d8c6f3e3c181f8fd3b5cf5d99c1
Azha Vianca de Belen <azhaviancadebelen@gmail.com>
2016-07-12 16:37:29 +0800

    fixed announcement css issues

--- application/views/Announcements_view.php
+++ application/views/Announcements_view.php
@@ -1,47 +1,60 @@
-<?php
-    if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-    $this->load->view ('Header');
-    $this->load->view ('Sidenav');
-?>
 
+
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
 <div class="row">
-    <div class="col s3 m3 l3"><br/></div>
+    <?php $this->load->view('Header');?>
+    <?php $this->load->view('Sidenav');?>
+    <div id="navarea" class="col s3 m3 l3 section"><br/><br/></div>
     <div class="col s9 m9 l9">
         <ul class="tabs">
             <li class="tab col s12"><a href="#annAddTab"> annAddTab </a></li>
             <li class="tab col s12"><a href="#annViewTab"> annViewTab </a></li>
         </ul>
     </div>
-    <div id="annAddTab" class="col s12">
-        <!-- Announcements Add -->
-        <?php
-            $this->load->helper ('form');
-            echo form_open ();
-            echo form_label ('Title', 'title');
-            echo form_input ('title', 'title');
-            echo form_label ('Text', 'text');
-            echo form_textarea ('text', 'text');
-            echo form_submit ();
-            echo form_reset ();
-            echo form_close ();
-        ?>
+    <div id="mainAppArea" class="col s9 m9 l9 section">
+        <div class="row">
+            <br/>
+            <br/>
+            <div id="annAddTab" class="col s12">
+                <!-- Announcements Add -->
+                <?php
+                    $this->load->helper ('form');
+                    echo form_open ();
+                    echo form_label ('Title', 'title');
+                    echo form_input ('title', 'title');
+                    echo form_label ('Text', 'text');
+                    echo form_textarea ('text', 'text');
+                    echo form_submit ();
+                    echo form_reset ();
+                    echo form_close ();
+                ?>
+            </div>
+            <br/>
+            <br/>
+        </div>
     </div>
-    <div id="annViewTab" class="col s12">
-        <!-- Announcements View -->
-        <?php
-            $lines = array ();
-            foreach ($announcements as $row)
-            {
-                $line = '<div class = "collapsible-header blue-grey darken-1 white-text">'.$row['title'].'</div><div class = "collapsible-body">'.$row['details'].'</div>';
-                $lines[] = $line;
-            }
-            $attributes = array ('class' => 'collapsible', 'data-collapsible' => 'accordion');
-            echo ul ($lines, $attributes);
-        ?>
+    <div id="mainAppArea" class="col s9 m9 l9 section">
+        <div class="row">
+            <div id="annViewTab" class="col s12">
+                <!-- Announcements View -->
+                <?php
+                    $lines = array ();
+                    foreach ($announcements as $row)
+                    {
+                        $line = '<div class = "collapsible-header blue-grey darken-1 white-text">'.$row['title'].'</div><div class = "collapsible-body">'.$row['details'].'</div>';
+                        $lines[] = $line;
+                    }
+                    $attributes = array ('class' => 'collapsible', 'data-collapsible' => 'accordion');
+                    echo ul ($lines, $attributes);
+                ?>
+            </div> 
+            <br/>
+            <br/>
+        </div>
     </div>
+
 </div>
-
-
+    
 <?php
     $this->load->view ('Common_scripts');
     $this->load->view ('Logout_script');
