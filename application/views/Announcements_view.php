4b9fdb77ab74512020d9a6d350339682ba28e5fc
clstia <janelliseurolfan@gmail.com>
2016-07-12 15:54:30 +0800

    Fixing "Announcements" tab

--- application/views/Announcements_view.php
+++ application/views/Announcements_view.php
@@ -1,51 +1,32 @@
 <?php
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-$this->load->view ('Header');
+    if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+    $this->load->view ('Header');
+    $this->load->view ('Sidenav');
 ?>
 
 <div class="row">
-    <?php
-        session_start ();
-        $this->load->view('Sidenav');
-    ?>
-    <!-- FILLER TO PUSH MAIN CONTENT TO THE RIGHT -->
-    <div class="col s3 m3 l3"><br/><br/></div>
-    <!-- MAIN CONTENT CONTAINER -->
-
-    <div id="annAddTab">
+    <div class="col s3 m3 l3"><br/></div>
+    <div class="col s9 m9 l9">
+        <ul class="tabs">
+            <li class="tab col s12"><a href="#annAddTab"> annAddTab </a></li>
+            <li class="tab col s12"><a href="#annViewTab"> annViewTab </a></li>
+        </ul>
     </div>
-    <div id="annMainTab" class="col s9 m9 l9">
-        <br/><br/>
-        <div class="row">
-            <div class="col s1 m1 l1">&nbsp;</div>
-            <div class="col s9 m9 l9 center-align">
-                <?php
-                    $lines = array ();
-                    foreach ($announcements as $row)
-                    {
-                        $line = '<div class = "collapsible-header blue-grey darken-1 white-text">'.$row['title'].'</div><div class = "collapsible-body">'.$row['details'].'</div>';
-                        $lines[] = $line;
-                    }
-                    $attributes = array ('class' => 'collapsible', 'data-collapsible' => 'accordion');
-                    echo ul ($lines, $attributes);
-                ?>
-            </div>
-            <div class="col s1 m1 l1">&nbsp;</div>
-            <div class="row"></div>
-        </div>
-    </div>
-    <!-- MAIN CONTENT CONTAINER END -->
+    <div id="annAddTab" class="col s12"> annAddTab </div>
+    <div id="annViewTab" class="col s12"> annViewTab </div>
 </div>
 
+
 <?php
-$this->load->view ('Common_scripts');
-$this->load->view ('Logout_script');
+    $this->load->view ('Common_scripts');
+    $this->load->view ('Logout_script');
 ?>
 <script type="text/javascript">
     $('document').ready(
         function ()
         {
             $('#annButton').addClass("black");
+            $('ul.tabs').tabs();
         }
     );
 </script>
