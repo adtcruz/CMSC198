1867da10b960dc87616d92b0ea8492dafa7a8aa7
adtcruz <atcruz1@up.edu.ph>
2016-07-13 14:11:19 +0800

    Documentation for the API that resets the password of an account

--- application/controllers/Reset_account_password_controller.php
+++ application/controllers/Reset_account_password_controller.php
@@ -1,24 +1,40 @@
 <?php
+//this is the controller for the API that resets the password of an account
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Reset_account_password_controller extends CI_Controller
 {
 	public function index ()
 	{
+		//session start to access session variables
 		session_start();
+
+		//checks if the type array key exists in session
 		if(array_key_exists("type",$_SESSION))
 		{
+
+			//checks if username array key exists in post
 			if(array_key_exists("username",$_POST))
 			{
+
+				//checks if password array key exists in post
 				if(array_key_exists("password",$_POST))
 				{
+					//if the username specified in post is the same as the username of the user in session
+					//do no proceed
 					if($_POST["username"]===$_SESSION["username"])
 					{
 						return;
 					}
+
 					else
 					{
+						//loads the code igniter database module
 						$this->load->database();
+
+						//checks if the username is found in the superadmin table
+						//if it is found change the password and record the action to the logs
+						//and return a message that the account password was reset
 						if($this->db->query("SELECT COUNT(superAdminID) FROM superAdmin WHERE BINARY username='".$_POST["username"]."'")->row_array()["COUNT(superAdminID)"]==1)
 						{
 							$this->db->query("UPDATE superAdmin SET password=SHA1('".$_POST["password"]."') WHERE BINARY username='".$_POST["username"]."'");
@@ -28,6 +44,9 @@ class Reset_account_password_controller extends CI_Controller
 						}
 						else
 						{
+							//checks if the username is found in the admin table
+							//if it is found change the password and record the action to the logs
+							//and return a message that the account password was reset
 							if($this->db->query("SELECT COUNT(adminID) FROM adminAcc WHERE BINARY username='".$_POST["username"]."'")->row_array()["COUNT(adminID)"]==1)
 							{
 								$this->db->query("UPDATE adminAcc SET password=SHA1('".$_POST["password"]."') WHERE BINARY username='".$_POST["username"]."'");
@@ -37,6 +56,9 @@ class Reset_account_password_controller extends CI_Controller
 							}
 							else
 							{
+								//checks if the username is found in the client table
+								//if it is found change the password and record the action to the logs
+								//and return a message that the account password was reset
 								if($this->db->query("SELECT COUNT(clientID) FROM client WHERE BINARY username='".$_POST["username"]."'")->row_array()["COUNT(clientID)"]==1)
 								{
 									$this->db->query("UPDATE client SET password=SHA1('".$_POST["password"]."') WHERE BINARY username='".$_POST["username"]."'");
