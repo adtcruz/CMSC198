351bdee5bd604af746c57fef0dd71a4dfdce8c51
clstia <janelliseurolfan@gmail.com>
2016-07-13 14:56:55 +0800

    Fixed "Add Announcements" module

--- application/controllers/Announcements_controller.php
+++ application/controllers/Announcements_controller.php
@@ -13,21 +13,36 @@ class Announcements_controller extends CI_Controller
 
     public function index ()
     {
-        $this->form_validation->set_rules ('title', 'Title', 'trim|required|min_length[10]|max_length[128]', array(
-            'required' => 'Title is required', 'min_length' => 'Input is too short', 'max_length' => 'Input is too long'
-        ));
-        $this->form_validation->set_rules ('text', 'Text', 'trim|required|min_length[10]|max_length[1024]', array(
-            'required' => 'Text is required', 'min_length' => 'Input is too short', 'max_length' => 'Input is too long'
-        ));
+        if (array_key_exists ('type', $_SESSION))
+        {
+            switch ($_SESSION['type'])
+            {
+                case 'client':
+                    $db_data = $this->am->getAnnouncements ();
+                    $this->load->view ('Announcements_client_view', $db_data);
+                break;
+                case 'admin':
+                case 'technician':
+                case 'superadmin':
+                    $this->form_validation->set_rules ('title', 'Title', 'trim|required|min_length[10]', array(
+                        'required' => 'Title is required', 'min_length' => 'Title is too short'
+                    ));
+                    $this->form_validation->set_rules ('text', 'Text', 'trim|required|min_length[10]', array(
+                        'required' => 'Text is required', 'min_length' => 'Content is too short'
+                    ));
 
-        if ($this->form_validation->run() == FALSE)
-        {
-            $db_data = $this->am->getAnnouncements ();
-            $this->load->view ('Announcements_view', $db_data);
-        }
-        else
-        {
-            // target toggle modal or something
+                    if ($this->form_validation->run() == FALSE)
+                    {
+                        $db_data = $this->am->getAnnouncements ();
+                        $this->load->view ('Announcements_admin_view', $db_data);
+                    }
+                    else
+                    {
+                        // target toggle modal or something
+                    }
+                break;
+                default: // redirect
+            }
         }
     }
 
@@ -37,11 +52,14 @@ class Announcements_controller extends CI_Controller
         {
             if ($_SESSION['type'] != 'client')
             {
+                echo 'yay!';
                 $db_data['title'] = $this->input->post ('title');
-                $db_data['text'] = $this->input->post ('content');
+                $db_data['text'] = $this->input->post ('text');
                 $db_data['createdBy'] = $_SESSION['username'];
                 $db_data['createdByType'] = $_SESSION['type'];
-                $this->am->addAnnouncement ($db_data);
+                $status = $this->am->addAnnouncement ($db_data);
+                echo '<br/>'.$db_data['title'].' '.$db_data['text'].' '.$db_data['createdBy'].' '.$db_data['createdByType'];
+                echo '<br/>current status'.$status;
             }
         }
     }
