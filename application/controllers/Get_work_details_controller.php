44667bb2b020e31e7867c75d4b51ba78df198f25
adtcruz <atcruz1@up.edu.ph>
2016-07-13 13:25:22 +0800

    Documentation for the API that gets the details of a work

--- application/controllers/Get_work_details_controller.php
+++ application/controllers/Get_work_details_controller.php
@@ -1,4 +1,5 @@
 <?php
+//this is the controller for the API that gets the details of a work
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Get_work_details_controller extends CI_Controller
@@ -6,18 +7,25 @@ class Get_work_details_controller extends CI_Controller
 	public function index ()
 	{
 
+		//session start to access session variables
 		session_start();
 
+		//checks if the type array key exists in session
 		if(array_key_exists("type",$_SESSION)){
 
-			if(($_SESSION["type"]==="superadmin")||($_SESSION["type"]==="technician")){
+			//if the user in session is a superadmin, admin, or technician
+			if(($_SESSION["type"]==="superadmin")||($_SESSION["type"]==="technician")||($_SESSION["type"]==="admin")){
 
+				//checks if workID array key exists in post
         if(array_key_exists("workID",$_POST)){
 
+					//loads codeigniter database module
           $this->load->database();
 
+					//queries the database
   				$rows = $this->db->query("SELECT workDescription, workCost FROM work WHERE workID=".$_POST["workID"]."")->result_array();
 
+					//sends a JSON object containing the material details
   		    if(count($rows)==1){
             echo (json_encode(
 	                  array(
@@ -27,6 +35,7 @@ class Get_work_details_controller extends CI_Controller
 								 ));
           }
 
+					//preterminates the execution of the script
   				return;
         }
 			}
