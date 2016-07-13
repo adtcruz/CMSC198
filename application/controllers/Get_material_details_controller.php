00ff166cade8060d7654a4e3c4157b93aac2e5ef
adtcruz <atcruz1@up.edu.ph>
2016-07-13 11:20:34 +0800

    Documentation for the API that gets the details of a material

--- application/controllers/Get_material_details_controller.php
+++ application/controllers/Get_material_details_controller.php
@@ -1,4 +1,5 @@
 <?php
+//this is the controller for the API that gets the details of a material
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Get_material_details_controller extends CI_Controller
@@ -6,18 +7,25 @@ class Get_material_details_controller extends CI_Controller
 	public function index ()
 	{
 
+		//session start to access session variables
 		session_start();
 
+		//checks if the type array key exists in session
 		if(array_key_exists("type",$_SESSION)){
 
-			if(($_SESSION["type"]==="superadmin")||($_SESSION["type"]==="technician")){
+			//if the user in session is a superadmin, admin, or technician
+			if(($_SESSION["type"]==="superadmin")||($_SESSION["type"]==="technician")||($_SESSION["type"]==="admin")){
 
+				//checks if materialID array key exists in post
         if(array_key_exists("materialID",$_POST)){
 
+					//loads codeigniter database module
           $this->load->database();
 
+					//queries the database
   				$rows = $this->db->query("SELECT materialName, materialCost, materialUnitMeasurement, materialDescription FROM materials WHERE materialID=".$_POST["materialID"]."")->result_array();
 
+					//sends a JSON object containing the material details
   		    if(count($rows)==1){
             echo (json_encode(
 	                  array(
@@ -29,6 +37,7 @@ class Get_material_details_controller extends CI_Controller
 								 ));
           }
 
+					//preterminates the execution of the script
   				return;
         }
 			}
