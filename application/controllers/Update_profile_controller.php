c328dbf39b9964097f5094e8615607658d27a6a1
adtcruz <atcruz1@up.edu.ph>
2016-07-13 15:02:33 +0800

    Documentation for the API that updates the user profile

--- application/controllers/Update_profile_controller.php
+++ application/controllers/Update_profile_controller.php
@@ -1,58 +1,65 @@
 <?php
+//this is the controller for the API that updates the user profile
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Update_profile_controller extends CI_Controller
 {
   public function index(){
 
+    //session start to access session variables
     session_start();
 
+    //loads code igniter database module
     $this->load->database();
 
+    //if type is defined
     if(array_key_exists("type",$_SESSION)){
 
+      //if username is defined
       if(array_key_exists("username",$_SESSION)){
 
+        //if givenName is defined
         if(array_key_exists("givenName",$_POST)){
 
+          //if lastName is defined
           if(array_key_exists("lastName",$_POST)){
 
+            //if user in session is a client
+            //update the given and last names in the database
+            //update the session variables holding the given and last names
+            //record the action to logs
+            //sends a message that the profile was updated
             if($_SESSION["type"]==="client"){
-
               $this->db->query("UPDATE client SET givenName='".$_POST["givenName"]."',lastName='".$_POST["lastName"]."' WHERE username='".$_SESSION["username"]."'");
-
               $this->db->query("INSERT INTO userLogs(logText,logTimestamp) VALUES ('".$_SESSION["username"]." has changed their given or last name(s)',CURRENT_TIMESTAMP)");
-
               $_SESSION["givenName"] = $_POST["givenName"];
-
               $_SESSION["lastName"] = $_POST["lastName"];
-
               echo "Profile updated";
             }
 
+            //if user in session is an admin or technician
+            //update the given and last names in the database
+            //update the session variables holding the given and last names
+            //record the action to logs
+            //sends a message that the profile was updated
             if(($_SESSION["type"]==="admin")||($_SESSION["type"]==="technician")){
-
               $this->db->query("UPDATE adminAcc SET givenName='".$_POST["givenName"]."',lastName='".$_POST["lastName"]."' WHERE username='".$_SESSION["username"]."'");
-
               $this->db->query("INSERT INTO userLogs(logText,logTimestamp) VALUES ('".$_SESSION["username"]." has changed their given or last name(s)',CURRENT_TIMESTAMP)");
-
               $_SESSION["givenName"] = $_POST["givenName"];
-
               $_SESSION["lastName"] = $_POST["lastName"];
-
               echo "Profile updated";
             }
 
+            //if user in session is a superadmin
+            //update the given and last names in the database
+            //update the session variables holding the given and last names
+            //record the action to logs
+            //sends a message that the profile was updated
             if($_SESSION["type"]==="superadmin"){
-
               $this->db->query("UPDATE superAdmin SET givenName='".$_POST["givenName"]."',lastName='".$_POST["lastName"]."' WHERE username='".$_SESSION["username"]."'");
-
               $this->db->query("INSERT INTO userLogs(logText,logTimestamp) VALUES ('".$_SESSION["username"]." has changed their given or last name(s)',CURRENT_TIMESTAMP)");
-
               $_SESSION["givenName"] = $_POST["givenName"];
-
               $_SESSION["lastName"] = $_POST["lastName"];
-
               echo "Profile updated";
             }
           }
