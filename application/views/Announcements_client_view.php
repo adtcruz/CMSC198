b63bfce13f62f8de2208716f9547410522241e6c
clstia <janelliseurolfan@gmail.com>
2016-07-13 14:59:33 +0800

    Added "Announcements" view for client users

---
+++ application/views/Announcements_client_view.php
@@ -0,0 +1,34 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+<div class="row">
+    <?php
+        $this->load->view('Header');
+        $this->load->view('Sidenav');
+    ?>
+    <div class="col s3 m3 l3"><br><br></div>
+    <div id="mainAppArea" class="col s9 m9 l9 section">
+        <div class="row">
+            <div class="col s12 m12 l12">
+                <!-- Announcements View -->
+                <?php
+                    $lines = array ();
+                    foreach ($announcements as $row)
+                    {
+                        $line = '<div class = "collapsible-header blue-grey darken-1 white-text">'.$row['title'].'</div><div class = "collapsible-body">'.$row['details'].'</div>';
+                        $lines[] = $line;
+                    }
+                    $attributes = array ('class' => 'collapsible', 'data-collapsible' => 'accordion');
+                    echo ul ($lines, $attributes);
+                ?>
+            </div>
+            <br/>
+        </div>
+    </div>
+</div>
+<?php
+    $this->load->view ('Common_scripts');
+    $this->load->view ('Logout_script');
+    $this->load->view ('Announcements_script');
+?>
+</body>
+
+</html>
