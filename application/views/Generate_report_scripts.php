a256cdb693d1e1776625a6098c64ad6265efdda1
clstia <janelliseurolfan@gmail.com>
2016-07-14 13:34:16 +0800

    Added line for calling "datePicker.js"

--- application/views/Generate_report_scripts.php
+++ application/views/Generate_report_scripts.php
@@ -1,5 +1,6 @@
 <script type = "text/javascript" src = "<?php echo base_url();?>assets/js/jquery-3.0.0.min.js"></script>
+<script type = "text/javascript" src = "<?php echo base_url();?>assets/js/materialize.min.js"></script>
 <script type = "text/javascript" src = "<?php echo base_url();?>assets/js/datePicker/picker.js"></script>
 <script type = "text/javascript" src = "<?php echo base_url();?>assets/js/datePicker/picker.date.js"></script>
 <script type = "text/javascript" src = "<?php echo base_url();?>assets/js/datePicker.js"></script>
-<script type="text/javascript" src="<?php base_url();?>assets/js/generate_report.controller.js"></script>
+<script type="text/javascript" src="<?php base_url();?>assets/js/Generate_report_controller.js"></script>
