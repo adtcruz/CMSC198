138cd48a7413358b925545b181125b3a0f38939d
adtcruz <atcruz1@up.edu.ph>
2016-07-14 15:39:41 +0800

    Deleted files and routes relating to Manage Application page(s)

--- application/views/Manage_application_script.php
+++
@@ -1,1 +0,0 @@
-<script type="text/javascript" src="<?php base_url();?>assets/js/Manage_application.controller.js"></script>
--- assets/js/Manage_application.controller.js
+++
@@ -1,5 +0,0 @@
-$('document').ready(
-	function(){
-		$("#mngeApButton").addClass("black");
-	}
-);
