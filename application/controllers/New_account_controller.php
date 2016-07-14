cf42a2f12768259df23686370726b61b7afc8293
clstia <janelliseurolfan@gmail.com>
2016-07-14 16:09:08 +0800

    Integrating "Select2"

--- application/controllers/New_account_controller.php
+++ application/controllers/New_account_controller.php
@@ -2,21 +2,23 @@
 
 class New_account_controller extends CI_Controller
 {
-  function index()
-  {
-    $this->load->model('Job_requests_model','jrm');
-    session_start();
-    $options = "";
-		if(array_key_exists("type",$_SESSION)){
-      if(($_SESSION["type"]==="technician")||($_SESSION["type"]==="admin")||($_SESSION["type"]==="superadmin")){
-        $options = $this->jrm->getOffices($_SESSION["type"]);
-  			$this->load->view('New_account_view', array('options' => $options));
-      }
+    function index()
+    {
+        $this->load->model('Job_requests_model','jrm');
+        session_start();
+        $options = "";
+        if(array_key_exists("type",$_SESSION))
+        {
+            if(($_SESSION["type"]==="technician")||($_SESSION["type"]==="admin")||($_SESSION["type"]==="superadmin"))
+            {
+                $options = $this->jrm->getOffices($_SESSION["type"]);
+                $this->load->view('New_account_view', array('options' => $options));
+            }
+        }
+        else
+        {
+            $this->load->view('Login_view');
+        }
     }
-
-    else{
-      $this->load->view('Login_view');
-    }
-  }
 }
 ?>
