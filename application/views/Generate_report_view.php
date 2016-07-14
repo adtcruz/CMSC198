a085ab0035100767f70d7910b0419a84610fbac3
clstia <janelliseurolfan@gmail.com>
2016-07-14 13:34:51 +0800

    Fixed view to match current format

--- application/views/Generate_report_view.php
+++ application/views/Generate_report_view.php
@@ -1,20 +1,20 @@
 <?php
     if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-    $this->load->view('Header');
-
-    if(!array_key_exists("username",$_SESSION))
+    
+    if (array_key_exists ("type", $_SESSION))
     {
-        $this->load->view('Login_page');
-    }
-    else
-    {
-        $this->load->view('Generate_report_view_form');
-    }
-    $this->load->view('Common_scripts');
-    if(!array_key_exists("username",$_SESSION)) $this->load->view('Login_script');
-    else{
-	       $this->load->view('Generate_report_scripts');
-	       $this->load->view('Logout_script');
+        if ($_SESSION['type'] == 'superadmin')
+        {
+            $this->load->view ('Header');
+            $this->load->view ('Generate_report_view_form');
+            $this->load->view ('Common_scripts');
+            $this->load->view ('Generate_report_scripts');
+            $this->load->view ('Logout_script');
+        }
+        else
+        {
+            $this->load->view ('Login_page');
+        }
     }
 ?>
 </body>
