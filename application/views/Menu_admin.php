8a7bf52e35f81a0c0b80ab0baa018bcaa0a7433f
clstia <janelliseurolfan@gmail.com>
2016-07-14 11:00:42 +0800

    Reflected file rename changes. Added "Generate Report" in "Manage Application" sub menu

--- application/views/Menu_admin.php
+++ application/views/Menu_admin.php
@@ -1,7 +1,6 @@
 <li id="viewJRButton"><a id="viewJRItem" class="waves-effect waves-light white-text" href="<?php base_url();?>job_requests">Job Requests</a></li>
 <li id="viewSchedButton"><a id="viewSchedItem" class="waves-effect waves-light white-text" href="<?php base_url();?>view_schedule">View Schedule</a></li>
 <li id="genBillButton"><a id="gnrtBlItem" class="waves-effect waves-light white-text" href="<?php base_url();?>generate_bill">Generate Bill</a></li>
-<li id="genBillButton"><a id="gnrtBlItem" class="waves-effect waves-light white-text" href="<?php base_url();?>generate_report">Generate Report</a></li>
 
 <li id="mngeApButton">
     <ul class="collapsible" data-collapsible="accordion">
@@ -24,8 +23,7 @@
                 <a class = "waves-effect waves-light grey darken-3 white-text" href = "<?php base_url();?>manage_accounts">Manage Accounts</a>
                 <a class = "waves-effect waves-light grey darken-3 white-text" href = "<?php base_url();?>manage_selectable_work">Manage Work</a>
                 <a class = "waves-effect waves-light grey darken-3 white-text" href = "<?php base_url();?>manage_selectable_materials">Manage Materials</a>
-
-                  
+                <a class="waves-effect waves-light grey darken-3 white-text" href="<?php base_url();?>generate_report">Generate Report</a>
             </div>
         </li>
     </ul>
