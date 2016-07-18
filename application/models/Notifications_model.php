62be6a57843746e253dfb5faf73389363f1d0ae0
clstia <janelliseurolfan@gmail.com>
2016-07-18 17:40:32 +0800

    Added "Get Read Notifications" on all users

--- application/models/Notifications_model.php
+++ application/models/Notifications_model.php
@@ -108,6 +108,52 @@
             return $table;
         }
 
+        public function getReadNotifs ($username, $type)
+        {
+            // get user ID
+            $userID = "";
+            switch ($type)
+            {
+                case 'client':
+                    $query = $this->db->query ('SELECT client.clientID FROM client WHERE (client.username = "'.$username.'")');
+                    $userID = $query->result_array ()[0]['clientID'];
+                break;
+                case 'admin':
+                case 'technician':
+                    $query = $this->db->query ('SELECT adminAcc.adminID FROM adminAcc WHERE (adminAcc.username = "'.$username.'")');
+                    $userID = $query->result_array ()[0]['adminID'];
+                break;
+                case 'superadmin':
+                    $query = $this->db->query ('SELECT superAdmin.superAdminID FROM adminAcc WHERE (superAdmin.username = "'.$username.'")');
+                    $userID = $query->result_array ()[0]['superAdminID'];
+                break;
+                default:
+                    redirect (base_url (), 'refresh');
+            }
+
+            // set table template
+            $this->table->set_template(array('table_open' =>'<table class="bordered centered highlight responsive-table">'));
+            $this->table->set_heading("Notification Details");
+
+            // get all notifs read where usertype is $type
+            $query = $this->db->query ('SELECT notifsRead.notifID, notifsRead.userID FROM notifsRead WHERE (notifsRead.userType = "'.$type.'")');
+            $notifs = $query->result_array ();
+
+            // foreach notifs, select those where userID = $userID and notifReadID = notifID (to get proper notifs)
+
+            foreach ($notifs as $row)
+            {
+                $query = $this->db->query ('SELECT notifsRead.userID, notifications.notifText FROM notifsRead, notifications WHERE (notifsRead.userID = '.$userID.') AND (notifsRead.notifID = notifications.notifID)');
+                if ($this->db->affected_rows () > 0)
+                {
+                    $this->table->add_row ($query->result_array ()[0]['notifText']);
+                }
+            }
+
+            $table = $this->table->generate ();
+            return $table;
+        }
+
         public function getUnreadCount ($username, $type)
         {
             $userID = '';
