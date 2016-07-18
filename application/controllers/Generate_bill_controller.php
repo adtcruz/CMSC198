4789b85b0c7ec488ca96c1b92458fe8e2aea405c
adtcruz <atcruz1@up.edu.ph>
2016-07-18 11:25:53 +0800

    Fixed indention

--- application/controllers/Generate_bill_controller.php
+++ application/controllers/Generate_bill_controller.php
@@ -12,13 +12,13 @@ class Generate_bill_controller extends CI_Controller
 
 	public function index ()
 	{
-        if(array_key_exists("type",$_SESSION))
-        {
-            $db_data = $this->gbm->getData ($_SESSION['username']);
-    		$this->load->view ('Generate_bill_view', $db_data);
+    if(array_key_exists("type",$_SESSION))
+    {
+	    $db_data = $this->gbm->getData ($_SESSION['username']);
+  		$this->load->view ('Generate_bill_view', $db_data);
 		}
 		else
-        {
+    {
 			$this->load->view('Login_view');
 		}
 	}
