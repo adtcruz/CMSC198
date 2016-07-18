bb4082a6cb373b02e86057329306377499c6419f
adtcruz <atcruz1@up.edu.ph>
2016-07-18 13:27:02 +0800

    Fixed code and messaging

--- application/models/Notifications_model.php
+++ application/models/Notifications_model.php
@@ -50,34 +50,31 @@
                     $userID = $query->result_array ()[0]['superAdminID'];
                 break;
             }
-            // get total notif counts
-            $query = $this->db->query ('SELECT COUNT(notifications.notifID) AS count FROM notifications');
-            $count = $query->result_array ()[0]['count'];
-
-            if($count == 0){
-              $db_data['unread'] = 0;
-              $db_data['unreadNotifs'] = "<h5 class='center-align'>Sorry, there are no notifications to display.</h5>";
-              return $db_data;
-            }
 
             $unread = 0;
             $temp = 0;
 
             $this->table->set_template(array('table_open' =>'<table class="bordered centered highlight responsive-table">'));
 
-            $this->table->set_heading("Notification Text","");
+            $this->table->set_heading("Notification Details","");
 
             $rows = $this->db->query('SELECT notifID FROM notifications')->result_array();
 
+            if($this->db->affected_rows() == 0){
+              $db_data['unread'] = 0;
+              $db_data['unreadNotifs'] = "<h5 class='center-align'>Sorry, there are no notifications to display.</h5>";
+              return $db_data;
+            }
+
             foreach($rows as $row){
 
-              $notifsQuery = $this->db->query('SELECT notifsRead.userID FROM notifsRead WHERE (notifsRead.notifID = '.$row['notifID'].') AND (notifsRead.userID = '.$userID.') AND (notifsRead.userType = "'.$type.'")');
+              $this->db->query('SELECT notifsRead.userID FROM notifsRead WHERE (notifsRead.notifID = '.$row['notifID'].') AND (notifsRead.userID = '.$userID.') AND (notifsRead.userType = "'.$type.'")');
 
               if($this->db->affected_rows() == 0){
 
                 $query2 = $this->db->query('SELECT notifications.notifText FROM notifications WHERE (notifications.notifID = '.$row['notifID'].')');
 
-                $this->table->add_row($query2->result_array()[0]['notifText'], '<a class = "btn blue-grey" onclick="markAsRead(\''.base_url().'\','.$row['notifID'].','.$userID.',\''.$type.'\')">Mark as Read</a>');
+                $this->table->add_row($query2->result_array()[0]['notifText'], '<a class = "waves-light waves-effect btn blue darken-4" onclick="markAsRead(\''.base_url().'\','.$row['notifID'].','.$userID.',\''.$type.'\')">Mark as Read</a>');
 
                 $unread++;
               }
