5b2d6ca89679360954b8964f6d1b5cc7eeb416c4
clstia <janelliseurolfan@gmail.com>
2016-07-18 18:03:39 +0800

    Resolved conflict

--- application/models/Notifications_model.php
+++ application/models/Notifications_model.php
@@ -7,6 +7,7 @@
             parent::__construct ();
             $this->load->database ();
         }
+
         public function notifBillGenerated ($username, $jobID)
         {
             // get client id using username
@@ -33,18 +34,14 @@
             $query = $this->db->query ('SELECT clientID FROM client WHERE (username = "'.$username.'")');
             $userID = $query->result_array ()[0]['clientID'];
             $unread = 0;
-
             $this->table->set_template(array('table_open' =>'<table class="bordered centered highlight responsive-table">'));
             $this->table->set_heading("Notification Details","");
-
             $rows = $this->db->query ('SELECT notifID FROM notifications WHERE (createdByType != \'client\')')->result_array();
-
             if($this->db->affected_rows() == 0){
               $db_data['unread'] = 0;
               $db_data['unreadNotifs'] = "<h5 class='center-align'>Sorry, there are no notifications to display.</h5>";
               return $db_data;
             }
-
             foreach ($rows as $row)
             {
                 $this->db->query ('SELECT notifsRead.notifID, notifsRead.userID FROM notifsRead WHERE (notifsRead.userType = \'client\') AND (notifsRead.notifID = '.$row['notifID'].') AND (notifsRead.userID = "'.$userID.'")');
@@ -55,7 +52,6 @@
                   $unread++;
                 }
             }
-
             if($unread == 0){
               $db_data['unread'] = 0;
               $db_data['unreadNotifs'] = "<h5 class='center-align'>Sorry, there are no notifications to display.</h5>";
@@ -64,6 +60,52 @@
             $db_data['unread'] = $unread;
             $db_data['unreadNotifs'] = $this->table->generate();
             return $db_data;
+        }
+
+        public function getReadNotifs ($username, $type)
+        {
+            // get user ID
+            $userID = "";
+            switch ($type)
+            {
+                case 'client':
+                    $query = $this->db->query ('SELECT client.clientID FROM client WHERE (client.username = "'.$username.'")');
+                    $userID = $query->result_array ()[0]['clientID'];
+                break;
+                case 'admin':
+                case 'technician':
+                    $query = $this->db->query ('SELECT adminAcc.adminID FROM adminAcc WHERE (adminAcc.username = "'.$username.'")');
+                    $userID = $query->result_array ()[0]['adminID'];
+                break;
+                case 'superadmin':
+                    $query = $this->db->query ('SELECT superAdmin.superAdminID FROM adminAcc WHERE (superAdmin.username = "'.$username.'")');
+                    $userID = $query->result_array ()[0]['superAdminID'];
+                break;
+                default:
+                    redirect (base_url (), 'refresh');
+            }
+
+            // set table template
+            $this->table->set_template(array('table_open' =>'<table class="bordered centered highlight responsive-table">'));
+            $this->table->set_heading("Notification Details");
+
+            // get all notifs read where usertype is $type
+            $query = $this->db->query ('SELECT notifsRead.notifID, notifsRead.userID FROM notifsRead WHERE (notifsRead.userType = "'.$type.'")');
+            $notifs = $query->result_array ();
+
+            // foreach notifs, select those where userID = $userID and notifReadID = notifID (to get proper notifs)
+
+            foreach ($notifs as $row)
+            {
+                $query = $this->db->query ('SELECT notifsRead.userID, notifications.notifText FROM notifsRead, notifications WHERE (notifsRead.userID = '.$userID.') AND (notifsRead.notifID = notifications.notifID)');
+                if ($this->db->affected_rows () > 0)
+                {
+                    $this->table->add_row ($query->result_array ()[0]['notifText']);
+                }
+            }
+
+            $table = $this->table->generate ();
+            return $table;
         }
 
         public function getUnreadCount ($username, $type)
