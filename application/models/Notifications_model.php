57d2c179c8050031b73670808c71a1eb9234b2c9
adtcruz <atcruz1@up.edu.ph>
2016-07-18 11:54:04 +0800

    Cleaned code

--- application/models/Notifications_model.php
+++ application/models/Notifications_model.php
@@ -62,18 +62,6 @@
 
             $unread = 0;
             $temp = 0;
-            // for each notifID, check if pairing (notifID, userID, userType) exists. if the pairing does not exist, add 1 to unread;
-            /*do
-            {
-              $temp++;
-              $query = $this->db->query ('SELECT notifsRead.userID FROM notifsRead WHERE (notifsRead.notifID = '.$temp.') AND (notifsRead.userID = '.$userID.') AND (notifsRead.userType = "'.$type.'")');
-              if ($this->db->affected_rows() == 0)
-              {
-                  $query2 = $this->db->query('SELECT notifications.notifText FROM notifications WHERE (notifications.notifID = '.$temp.')');
-                  $this->table->add_row($query2->result_array()[0]['notifText'], '<a class = "btn blue-grey" href = "'.base_url().'notifications/mark_as_read/'.$temp.'/'.$userID.'/'.$type.'">Mark as Read</a>');
-                  $unread++;
-              }
-            } while ($temp != $count);*/
 
             $this->table->set_template(array('table_open' =>'<table class="bordered centered highlight responsive-table">'));
 
@@ -82,6 +70,7 @@
             $rows = $this->db->query('SELECT notifID FROM notifications')->result_array();
 
             foreach($rows as $row){
+
               $notifsQuery = $this->db->query('SELECT notifsRead.userID FROM notifsRead WHERE (notifsRead.notifID = '.$row['notifID'].') AND (notifsRead.userID = '.$userID.') AND (notifsRead.userType = "'.$type.'")');
 
               if($this->db->affected_rows() == 0){
@@ -92,6 +81,12 @@
 
                 $unread++;
               }
+            }
+
+            if($unread == 0){
+              $db_data['unread'] = 0;
+              $db_data['unreadNotifs'] = "<h5 class='center-align'>Sorry, there are no notifications to display.</h5>";
+              return $db_data;
             }
 
             $db_data['unread'] = $unread;
