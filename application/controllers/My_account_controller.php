516434c5764f6c7a4453bb4318483c21435013ad
adtcruz <atcruz1@up.edu.ph>
2016-07-18 14:43:34 +0800

    Fixed issues in client my account view

--- application/controllers/My_account_controller.php
+++ application/controllers/My_account_controller.php
@@ -13,8 +13,9 @@ class My_account_controller extends CI_Controller
 	{
 		session_start();
 		if(array_key_exists("type",$_SESSION))
-        {
-            $unread = $this->nm->getUnreadCount ($_SESSION['username'], $_SESSION['type']);
+    {
+      if(($_SESSION["type"]==="admin")||($_SESSION["type"]==="technician")||($_SESSION["type"]==="superadmin"))$unread = $this->nm->getUnreadCount($_SESSION['username'], $_SESSION['type']);
+      else $unread['unread'] = 0;
 			$this->load->view('My_account_view', $unread);
 		}
 		else
