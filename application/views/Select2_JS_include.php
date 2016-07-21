594c18daf70f894aba62c5e7fc7d5876a7b8f786
adtcruz <atcruz1@up.edu.ph>
2016-07-21 11:06:15 +0800

    Added Navbar script to Select2_JS_Include

--- application/views/Select2_JS_include.php
+++ application/views/Select2_JS_include.php
@@ -3,6 +3,7 @@
 <script type = "text/javascript" src = "<?= base_url();?>assets/js/select2.full.min.js"></script>
 <link rel = "stylesheet" href = "<?= base_url ();?>assets/css/select2-materialize.css">
 <script type = "text/javascript" src = "<?= base_url();?>assets/js/materialize.min.js"></script>
+<script type = "text/javascript" src = "<?php echo base_url();?>assets/js/Navbar.controller.js"></script>
 
 <script type = "text/javascript">
     $(document).ready (function (){
