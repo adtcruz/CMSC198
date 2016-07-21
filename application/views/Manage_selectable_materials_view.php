65da822411b2ccbfb82e28a0d35a81d11c6ccba2
adtcruz <atcruz1@up.edu.ph>
2016-07-21 11:23:40 +0800

    Added Navbar to Manage_selectable_materials

--- application/views/Manage_selectable_materials_view.php
+++ application/views/Manage_selectable_materials_view.php
@@ -4,6 +4,8 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $this->load->view('Header');
 
+$this->load->view('Navbar');
+
 $this->load->view('Manage_selectable_materials_Dash', $unread);
 
 $this->load->view('Common_scripts');
