a019d3ebec723fc900747b66fa70b3968b053c35
adtcruz <atcruz1@up.edu.ph>
2016-07-21 11:37:55 +0800

    Removed logout button from Sidenav

--- application/views/Sidenav.php
+++ application/views/Sidenav.php
@@ -1,5 +1,5 @@
 <!-- SIDENAV -->
-<div class="col s3 m3 l3 side-nav fixed grey darken-4 white-text">
+<div id="slide-out" class="col s3 m3 l3 side-nav grey darken-4 white-text">
 	<div id="logo-container" class="center-align">
 		<p>
 			<h6>Welcome, <span id="sidebar_givenName"><?php echo $_SESSION["givenName"]."</span> <span id=\"sidebar_lastName\">".$_SESSION["lastName"];?></span>!
@@ -39,6 +39,5 @@
         }
 		?>
 		<li id="myAccountButton"><a id="myAccountItem" class="waves-effect waves-light white-text" href="<?php echo base_url();?>my_account">My Account</a></li>
-		<li><a class="waves-effect waves-light btn red darken-4 center-align white-text" onclick="logOut('<?php echo base_url();?>');">LOG OUT</a></li>
 	</ul>
 </div>
