dcc91d23739db0244f3340c4d6612d04fd1ab545
adtcruz <atcruz1@up.edu.ph>
2016-07-21 11:01:02 +0800

    Added Navbar from new-ui branch

---
+++ application/views/Navbar.php
@@ -0,0 +1,13 @@
+<div class="navbar-fixed">
+    <nav>
+      <div class="nav-wrapper grey darken-4">
+        <a href="<?php echo base_url();?>" class="brand-logo center">ITC Billing and Services System</a>
+        <ul class="left">
+          <li><a id="menuToggleButton" data-activates="slide-out" class="btn-floating btn-large btn-flat"><i class="large material-icons">view_list</i></a></li>
+        </ul>
+        <ul class="right">
+          <li><a class="waves-effect waves-light btn red darken-4 center-align white-text" onclick="logOut('<?php echo base_url();?>');">LOG OUT</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
