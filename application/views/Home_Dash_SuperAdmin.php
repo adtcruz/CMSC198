7fe7d1702c133f40caa20417dff6b3997cb25524
adtcruz <atcruz1@up.edu.ph>
2016-07-21 15:37:43 +0800

    Fixed Super Admin home

--- application/views/Home_Dash_SuperAdmin.php
+++ application/views/Home_Dash_SuperAdmin.php
@@ -1,38 +1,49 @@
 <div class="row">
 	<?php $this->load->view('Sidenav', $unread);?>
 	<div class="container">
-		<div class="row">
-	    <div class = "hide">
-        <span id="totalProcessed"><?=$total['processed'];?></span>
-        <span id="totalCanceled"><?=$total['canceled'];?></span>
-        <span id="totalProcessing"><?=$total['processing'];?></span>
-        <span id="totalPending"><?=$total['pending'];?></span>
-	    </div>
-	    <div id="jobStatusChart"></div>
+		<!-- job status chart start -->
+		<div class="col s12 m12 l12">
+			<div class="card grey lighten-2">
+				<div class="card-content white-text">
+					<div id="jobStatusChart"></div>
+				</div>
+			</div>
 		</div>
-
-    <div class="row">
-	    <div id="pieData" class="hide">
-	        <?php
-	            echo json_encode ($values);
-	        ?>
-	    </div>
-	    <div id="totalWork"></div>
+		<!-- job status chart end -->
+		<!-- total work chart start -->
+		<div class="col s12 m12 l12">
+			<div class="card grey lighten-2">
+				<div class="card-content white-text">
+					<div id="totalWork"></div>
+				</div>
+			</div>
 		</div>
-
-        <div class="row">
-			<div class="col s1 m1 l1">&nbsp;</div>
-			<div class="col s10 m10 l10">
-                <div id="incomeData" class="hide">
-                    <?php
-                        echo json_encode ($income);
-                    ?>
-                </div>
-                <div id="monthlyIncome"></div>
-            </div>
+		<!-- total work chart end -->
+		<!-- monthly income chart start -->
+		<div class="col s12 m12 l12">
+			<div class="card grey lighten-2">
+				<div class="card-content white-text">
+					<div id="monthlyIncome"></div>
+				</div>
+			</div>
 		</div>
+		<!-- monthly income chart end -->
 	</div>
 </div>
 
-</body>
-</html>
+<div class = "hide">
+	<span id="totalProcessed"><?=$total['processed'];?></span>
+	<span id="totalCanceled"><?=$total['canceled'];?></span>
+	<span id="totalProcessing"><?=$total['processing'];?></span>
+	<span id="totalPending"><?=$total['pending'];?></span>
+</div>
+<div id="pieData" class="hide">
+	<?php
+			echo json_encode ($values);
+	?>
+</div>
+<div id="incomeData" class="hide">
+		<?php
+				echo json_encode ($income);
+		?>
+</div>
