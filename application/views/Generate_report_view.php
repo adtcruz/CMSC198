b0ac00e79b6d0f7981d4b549be68b2af6f6ae186
adtcruz <atcruz1@up.edu.ph>
2016-07-21 11:18:42 +0800

    Added Navbar to Generate Report

--- application/views/Generate_report_view.php
+++ application/views/Generate_report_view.php
@@ -6,6 +6,7 @@
         if ($_SESSION['type'] == 'superadmin')
         {
             $this->load->view ('Header');
+            $this->load->view ('Navbar');  
             $this->load->view ('Generate_report_view_form', $unread);
             $this->load->view ('Common_scripts');
             $this->load->view ('Generate_report_scripts');
