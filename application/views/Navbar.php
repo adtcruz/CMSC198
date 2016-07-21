9d53076d9679ecc39b479cad7ff4770ec1bb31d2
adtcruz <atcruz1@up.edu.ph>
2016-07-21 16:27:49 +0800

    Made navbar responsive

--- application/views/Navbar.php
+++ application/views/Navbar.php
@@ -1,12 +1,14 @@
 <div class="navbar-fixed">
   <nav>
     <div class="nav-wrapper grey darken-4">
-      <a href="<?php echo base_url();?>" class="brand-logo center">ITC Networking Services and Billing System</a>
+      <a href="<?php echo base_url();?>" class="brand-logo center hide-on-small-only">ITC Networking Services and Billing System</a>
+      <h6 href="<?php echo base_url();?>" class="brand-logo center hide-on-med-and-up">ITC Net. Svc. &amp; Billing</h6>
       <ul class="left">
         <li><a id="menuToggleButton" data-activates="slide-out" class="btn-floating btn-large btn-flat"><i class="large material-icons">view_list</i></a></li>
       </ul>
       <ul class="right">
-        <li><a class="waves-effect waves-light btn dropdown-button grey darken-3 center-align white-text" data-activates="user-dropdown"><?php echo $_SESSION["givenName"]." ".$_SESSION["lastName"];?> ▼</a></li>
+        <li class="hide-on-small-only"><a class="waves-effect waves-light btn dropdown-button grey darken-3 center-align white-text" data-activates="user-dropdown"><?php echo $_SESSION["givenName"]." ".$_SESSION["lastName"];?> ▼</a></li>
+        <li class="hide-on-med-and-up"><a class="btn-floating btn dropdown-button grey darken-4 center-align white-text" data-activates="user-dropdown">▼</a></li>
       </ul>
       <ul id="user-dropdown" class="dropdown-content">
         <?php
