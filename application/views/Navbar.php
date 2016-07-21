cbec3e2884b3bc30214ce3eaded189d1ad09931e
adtcruz <atcruz1@up.edu.ph>
2016-07-21 16:46:19 +0800

    Fixed responsive dropdown

--- application/views/Navbar.php
+++ application/views/Navbar.php
@@ -8,9 +8,17 @@
       </ul>
       <ul class="right">
         <li class="hide-on-small-only"><a class="waves-effect waves-light btn dropdown-button grey darken-3 center-align white-text" data-activates="user-dropdown"><?php echo $_SESSION["givenName"]." ".$_SESSION["lastName"];?> ▼</a></li>
-        <li class="hide-on-med-and-up"><a class="btn-floating btn dropdown-button grey darken-4 center-align white-text" data-activates="user-dropdown">▼</a></li>
+        <li class="hide-on-med-and-up"><a class="btn-floating btn dropdown-button grey darken-4 center-align white-text" data-activates="mob-dropdown">▼</a></li>
       </ul>
       <ul id="user-dropdown" class="dropdown-content">
+        <?php
+        if($_SESSION["type"]==="client") echo '<li><a href="'.base_url().'job_requests">My Job Requests</a></li>';
+        ?>
+        <li><a href="<?php echo base_url();?>my_account">My Account</a></li>
+        <li class="divider"></li>
+        <li><a onclick="logOut('<?php echo base_url();?>');">Log Out</a></li>
+      </ul>
+      <ul id="mob-dropdown" class="dropdown-content">
         <?php
         if($_SESSION["type"]==="client") echo '<li><a href="'.base_url().'job_requests">My Job Requests</a></li>';
         ?>
