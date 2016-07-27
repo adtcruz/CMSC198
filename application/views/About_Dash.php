ee72b886a2838a318182eeb4b92206f0d0f0c9c6
Joanne Encarnacion <joanne14.encarnacion@gmail.com>
2016-07-27 11:42:05 +0800

    edited HOW TO and ABOUT THE SYSTEM

--- application/views/About_Dash.php
+++ application/views/About_Dash.php
@@ -8,12 +8,12 @@
 				<h4 class="center-align">About The System</h4>
 				<br/>
 				<p class="center-align">
-					The ITC Billing System (ITCBS) integrates the billing system for UPLB ITC’s System Administration, Network Administration and Technical Support sections.  
+					The ITC Network Services and Billing System (ITCNSBS) integrates the billing system for UPLB ITC’s System Administration, Network Administration and Technical Support sections.  
 					It caters to the need of the ITC team of a common system in managing job requests. 
 					The system uses CodeIgniter framework and MariaDB for faster job transactions and record management. 
 					The system provides an online version of the job request form and saves the input data to a database that can be manipulated using the system. 
 					The main functionalities are already operational and ready for deployment but the system is still under further development. 
-					The ITCBS is accessible via the intranet and is hosted via a local server.	
+					The ITCNSBS is accessible via the intranet and is hosted via a local server.	
 				</p>
 			</div>
 				
